f6861307ffd0df3b45710e8a7fb4a868a9e42d3c
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-05 10:29:36 -0400

    created new repo

---
+++ shared-ui/style/colors.ts
@@ -0,0 +1,7 @@
+const colors = {
+    WHITE: '#FFFFFF',
+    BLACK: '#000000'
+  };
+  
+  export { colors };
+  
---
+++ shared-ui/style/icons.ts
@@ -0,0 +1,6 @@
+const icons = {
+    HACKBEANPOT_LOGO: '/assets/hackbeanpot-logo.png',
+  };
+  
+  export { icons };
+  
