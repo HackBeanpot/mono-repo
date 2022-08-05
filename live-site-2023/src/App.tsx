0f678d915f5910c1c5f8f2e46d5a608ed51147d0
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-05 18:31:56 -0400

    changed live site to use gatsby

--- live-site-2023/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div>Welcome to our live site!!!!!!</div>;
-}
-
-export default App;
--- main-site-2023/src/pages/index.tsx
+++ main-site-2023/src/pages/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
+import Button from '../../../shared-ui/components/Button';
 
 const IndexPage = () => {
-  return <div>Welcome to our main site!!!!</div>;
+  return (
+    <div>
+      Welcome to our main site!!!! <Button />
+    </div>
+  );
 };
 
 export default IndexPage;
