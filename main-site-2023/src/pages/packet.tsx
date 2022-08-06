f4a0c40f28319f592237c098ffbd0262924f8280
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-05 20:27:18 -0400

    added page for sponsib packet

---
+++ main-site-2023/src/pages/packet.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import type { HeadFC } from 'gatsby';
+
+const PacketPage = () => {
+  return <div>Sponsib packet!!!!</div>;
+};
+
+export default PacketPage;
+
+export const Head: HeadFC = () => <title>Sponsorship Packet</title>;
