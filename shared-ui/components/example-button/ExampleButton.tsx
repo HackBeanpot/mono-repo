bb9e014bac29cd1ba40825c200711e6706a34e65
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-09 18:44:10 -0400

    responsiveness is working

--- shared-ui/components/example-button/ExampleButton.tsx
+++ shared-ui/components/example-button/ExampleButton.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StyledExampleButton } from './ExampleButton.style';
 
+function blah() {
+
+}
+
 export default function ExampleButton() {
   return <StyledExampleButton>Example button!!!</StyledExampleButton>;
 }
