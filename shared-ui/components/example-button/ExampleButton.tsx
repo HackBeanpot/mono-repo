6f55ac03d219948da3574b1471940de7e099eb21
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-09 18:46:17 -0400

    removed cjs file

--- shared-ui/components/example-button/ExampleButton.tsx
+++ shared-ui/components/example-button/ExampleButton.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { StyledExampleButton } from './ExampleButton.style';
 
-function blah() {
-
-}
-
 export default function ExampleButton() {
   return <StyledExampleButton>Example button!!!</StyledExampleButton>;
 }
