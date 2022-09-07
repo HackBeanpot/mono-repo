c9f8a5217ba87d049bf4d3e623eafdbf1c8cfef2
Judy Zhang <59738880+jzhzhang66@users.noreply.github.com>
2022-09-07 16:31:30 -0400

    Added colors  (#TICKET_NUMBER)
    Added colors constant to store all colors in one object

--- main-site/src/pages/index.tsx
+++ main-site/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
 import ExampleButton from '../../../shared-ui/components/example-button/ExampleButton';
+import Logo from '../../../shared-ui/images/hackbeanpot-logo.png';
 
 const IndexPage: React.FC = () => {
   return (
     <div>
       Welcome to our main site!!! <ExampleButton />
+      <img src={Logo} />
     </div>
   );
 };
