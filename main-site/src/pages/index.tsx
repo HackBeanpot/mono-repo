53b1db87059d426d3d2bdd0280c12b28fc1d64fe
jzhzhang66 <jzhzhang66@gmail.com>
2022-09-09 22:52:58 -0400

    kind of working

--- main-site/src/pages/index.tsx
+++ main-site/src/pages/index.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import type { HeadFC } from 'gatsby';
 import ExampleButton from '../../../shared-ui/components/example-button/ExampleButton';
 import Logo from '../../../shared-ui/images/hackbeanpot-logo.png';
+//import { H1 } from '../../../shared-ui/style/typography';
+import '../index.css';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-family: 'Calistoga-Regular';
+`;
 
 const IndexPage: React.FC = () => {
   return (
     <div>
       Welcome to our main site!!! <ExampleButton />
       <img src={Logo} />
+      <H1>Desert Exploration</H1>
     </div>
   );
 };
---
+++ shared-ui/style/typography.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { min } from '../lib/responsive';
+import { colors } from './colors';
+
+// desert exploration
+const H1 = styled.h1`
+  fontfamily: 'Calistoga-Regular';
+`;
+
+// Hackbeanpot is about...
+const H2 = styled.h2``;
+
+// Explore the desert with Hackbeanpot
+const H3 = styled.h3``;
+
+// Explore the desert with Hackbeanpot
+const H4 = styled.h4`
+  color: ${(props): string =>
+    props.color ? props.color : colors.BUTTON_GREEN};
+`;
+
+export { H1 };
