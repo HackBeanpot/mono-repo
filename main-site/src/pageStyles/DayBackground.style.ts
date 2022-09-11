92421d6461d3531187f5707c5c13f928f3ca05c5
jzhzhang66 <jzhzhang66@gmail.com>
2022-09-10 23:26:59 -0400

    fixed height / width issue

---
+++ main-site/src/pageStyles/DayBackground.style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledDayBackground = styled.img`
+  width: 100%;
+  position: absolute;
+  z-index: -1;
+`;
+
+export { StyledDayBackground };
--- main-site/src/pages/index.tsx
+++ main-site/src/pages/index.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import type { HeadFC } from 'gatsby';
 import ExampleButton from '../../../shared-ui/components/example-button/ExampleButton';
 import Logo from '../../../shared-ui/images/hackbeanpot-logo.png';
-import StyledDayBackground  from '../../../shared-ui/components/day-background/DayBackground.style';
+import { StyledDayBackground } from '../pageStyles/DayBackground.style';
+import DayBackground from '../../../shared-ui/images/day-background.svg';
 
 const IndexPage: React.FC = () => {
   return (
     <div>
-      <StyledDayBackground >
-        Welcome to our main site!!! <ExampleButton />
-        <img src={Logo} />
-      </StyledDayBackground >
+      <StyledDayBackground src={DayBackground} />
+      Welcome to our main site!!! <ExampleButton />
+      <img src={Logo} />
     </div>
   );
 };
