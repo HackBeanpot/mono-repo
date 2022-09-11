6f4e67527548c7301186c5c40724bc6629533a69
jzhzhang66 <jzhzhang66@gmail.com>
2022-09-10 23:59:31 -0400

    created styled components folder

--- main-site/src/pages/index.tsx
+++ main-site/src/pages/index.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
-import ExampleButton from '../../../shared-ui/components/example-button/ExampleButton';
-import Logo from '../../../shared-ui/images/hackbeanpot-logo.png';
-import { StyledDayBackground } from '../pageStyles/DayBackground.style';
+import { StyledDayBackground } from '../../../shared-ui/styled-components/day-background/background.style';
 import DayBackground from '../../../shared-ui/images/day-background.svg';
 
 const IndexPage: React.FC = () => {
   return (
     <div>
       <StyledDayBackground src={DayBackground} />
-      Welcome to our main site!!! <ExampleButton />
-      <img src={Logo} />
     </div>
   );
 };
