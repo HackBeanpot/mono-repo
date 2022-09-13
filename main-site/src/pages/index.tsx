897755cdbbc0e6a0f1f434ad558161dc975bb7b8
karynayen <karynayen@gmail.com>
2022-09-13 19:36:12 -0400

    last minute fixes

--- main-site/src/pages/index.tsx
+++ main-site/src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { HeadFC } from 'gatsby';
 import useMatchMedia from 'react-use-match-media';
 import '../../../shared-ui/style/global.css';
 import { min } from '../../../shared-ui/lib/responsive';
-import { StyledDayBackground } from '../../../shared-ui/styled-components/day-background.style';
+import { StyledDayBackground } from '../../../shared-ui/styled-components/background.style';
 import DayBackgroundDesktop from '../../../shared-ui/images/day-background-desktop.svg';
 import DayBackgroundMobile from '../../../shared-ui/images/day-background-mobile.svg';
 
--- shared-ui/styled-components/background.style.ts
+++ shared-ui/styled-components/background.style.ts
@@ -4,9 +4,6 @@ const StyledDayBackground = styled.img`
   width: 100%;
   position: absolute;
   z-index: -1;
-  top: 0;
-  left: 0;
-  
 `;
 
 export { StyledDayBackground };
