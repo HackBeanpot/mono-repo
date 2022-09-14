7835bd4458f4fff533edf08cb5ffe5bdb2fefa94
Judy Zhang <59738880+jzhzhang66@users.noreply.github.com>
2022-09-14 09:37:27 -0400

    Typography (#6)
    Added fonts and typography styled components.

--- main-site/src/pages/index.tsx
+++ main-site/src/pages/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
+import { H1, H2, H3, H4, P } from '../../../shared-ui/style/typography';
+import '../../../shared-ui/index.css';
 import useMatchMedia from 'react-use-match-media';
 import '../../../shared-ui/style/global.css';
 import { min } from '../../../shared-ui/lib/responsive';
 import { StyledDayBackground } from '../../../shared-ui/styled-components/background.style';
 import DayBackgroundDesktop from '../../../shared-ui/images/day-background-desktop.svg';
 import DayBackgroundMobile from '../../../shared-ui/images/day-background-mobile.svg';
-
 
 const IndexPage: React.FC = () => {
   const isDesktop = useMatchMedia(min.tablet);
@@ -17,6 +18,20 @@ const IndexPage: React.FC = () => {
       ) : (
         <StyledDayBackground src={DayBackgroundMobile} />
       )}
+      <H1>Desert Exploration</H1>
+      <H2>Explore the desert with HackBeanpot!</H2>
+      <H3>HackBeanpot 2023</H3>
+      <H4>Code4Joy, 2022</H4>
+      <P>
+        HackBeanpot 2022 is gonna be out of this world! Join our community of
+        astronaut trainees* for a weekend of exploration, collaboration, and
+        fun! (Oh, and did we mention free food and swag?) Hackers can expect to
+        put their resourcefulness to the test, while sharing stories, and
+        learning new skills from peers. So whether you're a seasoned
+        hackathon-goer, an ‘I-have-never-written-a-line-of-code’ beginner, or
+        someone in-between, we’re excited for you to embark on this adventure
+        with us!
+      </P>
     </div>
   );
 };
