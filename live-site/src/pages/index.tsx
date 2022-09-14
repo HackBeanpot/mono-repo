5cf3ef5507318e64ead613206ba8b63b4a1dd287
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-09-13 20:40:58 -0400

    build fix

--- live-site/src/pages/index.tsx
+++ live-site/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
-import ExampleButton from '../../../shared-ui/components/example-button/ExampleButton';
 
 const IndexPage: React.FC = () => {
   return (
     <div>
-      Welcome to our live site!!!! <ExampleButton />
+      Welcome to our live site!!!! 
     </div>
   );
 };
