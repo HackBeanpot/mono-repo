4cf44adedd52a2678ea1996bdd3e53749fd281a3
Dean Frame <dacframe@gmail.com>
2022-09-19 19:30:01 -0400

    added all 3 columns - todo social media icons + responsive

--- shared-ui/components/footer/Footer.styles.ts
+++ shared-ui/components/footer/Footer.styles.ts
@@ -1,23 +1,63 @@
 import styled from 'styled-components';
 import { colors } from '../../style/colors';
+import { P, fonts } from '../../style/typography';
 
 const StyledFooter = styled.div`
   background-color: ${colors.HEADER_FOOTER_BLUE};
   display: flex;
   flex-direction: row;
-  height: 500px;
+  height: 368px;
+`
+
+const StyledLogoContainer = styled.div`
+  flex-basis: 20%;
 `
 
 const StyledHackBeanpotLogo = styled.img`
+  padding: 0.7em;
 `
 
 const StyledTabContainer = styled.div`
+  height: 100%;
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
+  align-self: center;
+  flex-basis: 40%;
+  padding: 125px 0 125px 150px;
+`
+
+const StyledTab = styled(P)`{
+  color: ${colors.WHITE};
+  font-family: ${fonts.nunitoSansSemibold};
+  padding: 0.15em;
+}
+`;
+
+const StyledButtonContainer = styled.div`
+  flex-basis: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 50px 0;
+`
+
+const StyledEmailInput = styled.input`
+  color: ${colors.HEADER_FOOTER_BLUE};
+  font-size: 1em;
+  background-color: ${colors.WHITE};
+  border: 1px solid white;
+  padding: 1em;
+  border-radius: 2em;
 `
 
 export {
   StyledFooter,
+  StyledLogoContainer,
   StyledHackBeanpotLogo,
-  StyledTabContainer
+  StyledTabContainer,
+  StyledTab,
+  StyledButtonContainer,
+  StyledEmailInput
 }
--- shared-ui/components/footer/Footer.tsx
+++ shared-ui/components/footer/Footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FooterProps, TabInfo } from '../../lib/types';
-import { StyledFooter, StyledHackBeanpotLogo, StyledTabContainer } from './Footer.styles';
+import { StyledButtonContainer, StyledEmailInput, StyledFooter, StyledHackBeanpotLogo, StyledTab, StyledTabContainer } from './Footer.styles';
 import HackBeanpotWhiteLogo from '../../images/hackbeanpot-white-logo.png';
+import PrimaryButton from '../primary-button/PrimaryButton';
 
 const Footer: React.FC<FooterProps> = ({ tabs }) => {
+  const [email, setEmail] = useState<string>('');
 
   return (
     <StyledFooter>
@@ -14,12 +16,20 @@ const Footer: React.FC<FooterProps> = ({ tabs }) => {
         height='56px'
       />
       <StyledTabContainer>
-      {tabs.map((tab: TabInfo) => (
-        <div key={tab.name}>
-          {tab.name}
-        </div>
-      ))}
+        {tabs.map((tab: TabInfo) => (
+          <StyledTab>
+            {tab.name}
+          </StyledTab>
+        ))}
       </StyledTabContainer>
+      <StyledButtonContainer>
+        <StyledEmailInput
+          placeholder='Enter your email'
+          value={email}
+          onChange={(e: React.FormEvent<HTMLInputElement>): void => setEmail(e.currentTarget.value)}
+        />
+        <PrimaryButton btnText='Join our mailing list' btnLink='' />
+      </StyledButtonContainer>
     </StyledFooter>
   )
 }
