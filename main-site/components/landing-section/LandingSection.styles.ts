b90bae2bd1f1d0eaa511cbf7a9581a984b574c07
Surbhi Gulati <53664026+surbhi-gulati@users.noreply.github.com>
2022-09-27 00:31:51 -0700

    mobile

--- main-site/components/landing-section/LandingSection.styles.ts
+++ main-site/components/landing-section/LandingSection.styles.ts
@@ -1,11 +1,26 @@
 import styled from 'styled-components';
 import { max } from '../../../shared-ui/lib/responsive';
 import { colors } from '../../../shared-ui/style/colors';
-import { H3 } from '../../../shared-ui/style/typography';
+import { H1, H3 } from '../../../shared-ui/style/typography';
 
-const H3White = styled(H3)`{
-    color: ${colors.WHITE};
-    padding-bottom: 1em;
+const StyledHackathonText = styled(H3) `{
+  color: ${colors.WHITE};
+  padding-bottom: 1em;
+  @media ${max.tablet} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+`;
+
+const StyledThemeText = styled(H1) `{
+color: ${colors.WHITE};
+  @media ${max.tablet} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 `;
 
@@ -16,27 +31,21 @@ const StyledLandingSectionContainer = styled.div`
   @media ${max.tablet} {
     padding-top: 20em;
     margin: 20em;
-    display: flex;
-    margin-top: 20em;
-    justify-content: center;
-    align-items: center;
   }
 `;
 
 const StyledLandingButtonContainer = styled.div`
-padding-top: 3em;
-  @media ${max.tablet} {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  padding-top: 3em;
+    @media ${max.tablet} {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
 `;
-// TODO: IMPORT SUN/MOON ICONS DEPENDING ON WHICH
 const StyledToggle = styled.img`
-float: right;
-padding-top: 5.5em;
-padding-right: 0.5em;
-
+  float: right;
+  padding-top: 5.5em;
+  padding-right: 0.5em;
 `;
 
-export { StyledLandingButtonContainer, StyledLandingSectionContainer, H3White, StyledToggle };
+export { StyledHackathonText, StyledThemeText, StyledLandingButtonContainer, StyledLandingSectionContainer, StyledToggle };
