4abbace5ecc903e9383946ee849c46a166802666
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-10-04 23:00:34 -0400

    code of conduct responsiveness

--- main-site/src/page-styles/code-of-conduct.styles.ts
+++ main-site/src/page-styles/code-of-conduct.styles.ts
@@ -3,75 +3,47 @@ import { colors } from '../../../shared-ui/style/colors';
 import { H2, P } from '../../../shared-ui/style/typography';
 import { max } from '../../../shared-ui/lib/responsive';
 
-const StyledCodeOfConductContent = styled(P)`
-  @media ${max.tablet} {
-    padding-left: 20%;
-  }
-`;
-const StyledBackground = styled.div`
-  width: 100%;
-  background-color: ${colors.DUNE_PRIMARY};
-  margin-top: 0;
+const StyledTextContainer = styled.div`
+  margin: 0 6em;
+  padding: 8em 0;
 
   @media ${max.tablet} {
-    width: 100%;
-    padding-left: 0;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    margin: 0 2em;
   }
+`;
+
+const StyledBodyTextContainer = styled.div`
+  padding-left: 5em;
+
+  @media ${max.tablet} {
+    padding-left: 0;
+  }
+`;
+
+const StyledBackground = styled.div`
+  background-color: ${colors.DUNE_PRIMARY};
 `;
 
 const StyledParagraph = styled(P)`
   color: ${colors.TEXT_BOX};
-  padding-left: 20%;
-  padding-right: 7em;
   line-height: 1.8em;
-
-  @media ${max.tablet} {
-    padding-left: 3em;
-    width: 90%;
-  }
-  @media ${max.mobile} {
-    padding-left: 1.7em;
-    width: 100%;
-  }
+  padding-bottom: 1em;
 `;
 
 const StyledHeading = styled(H2)`
-  padding-left: 4em;
-  padding-top: 3em;
   padding-bottom: 1.5em;
   color: ${colors.TEXT_BOX};
-
-  @media ${max.tablet} {
-    padding-left: 1em;
-  }
 `;
 const StyledDirections = styled(P)`
   padding-top: 2em;
-  padding-left: 20%;
   color: ${colors.TEXT_BOX};
   font-size: 1.875em;
-
-  @media ${max.tablet} {
-    padding-left: 1.8em;
-    width: 100%;
-  }
 `;
 
 const StyledParagraphBottom = styled(P)`
   color: ${colors.TEXT_BOX};
-  padding-left: 20%;
-  padding-right: 7em;
   line-height: 1.9em;
   padding-bottom: 5em;
-
-  @media ${max.tablet} {
-    padding-left: 3em;
-    width: 100%;
-  }
 `;
 
 export {
@@ -80,5 +52,6 @@ export {
   StyledDirections,
   StyledBackground,
   StyledParagraphBottom,
-  StyledCodeOfConductContent
+  StyledTextContainer,
+  StyledBodyTextContainer
 };
