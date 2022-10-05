cf1eb33256f9da75bb9c1281576eec153fa6bdb2
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-10-04 22:22:15 -0400

    alignment for code of conduct

--- main-site/src/pages/code-of-conduct.styles.ts
+++ main-site/src/pages/code-of-conduct.styles.ts
@@ -3,53 +3,74 @@ import { colors } from '../../../shared-ui/style/colors';
 import { H2, P } from '../../../shared-ui/style/typography';
 import { max } from '../../../shared-ui/lib/responsive';
 
+const StyledCodeOfConductContent = styled(P)`
+  @media ${max.tablet} {
+    padding-left: 20%;
+  }
+`;
 const StyledBackground = styled.div`
-  width: em;
+  width: 100%;
   background-color: ${colors.DUNE_PRIMARY};
   margin-top: 0;
+
+  @media ${max.tablet} {
+    width: 100%;
+    padding-left: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 const StyledParagraph = styled(P)`
   color: ${colors.TEXT_BOX};
-  padding-left: 15em;
+  padding-left: 20%;
   padding-right: 7em;
   line-height: 1.8em;
 
   @media ${max.tablet} {
-    padding-left: 2em;
+    padding-left: 3em;
+    width: 90%;
+  }
+  @media ${max.mobile} {
+    padding-left: 1.7em;
+    width: 100%;
   }
 `;
 
 const StyledHeading = styled(H2)`
-  padding-left: 2em;
+  padding-left: 4em;
   padding-top: 3em;
   padding-bottom: 1.5em;
   color: ${colors.TEXT_BOX};
 
   @media ${max.tablet} {
-    padding-left: 1.5em;
+    padding-left: 1em;
   }
 `;
 const StyledDirections = styled(P)`
   padding-top: 2em;
-  padding-left: 10em;
+  padding-left: 20%;
   color: ${colors.TEXT_BOX};
   font-size: 1.875em;
 
   @media ${max.tablet} {
-    padding-left: 1em;
+    padding-left: 1.8em;
+    width: 100%;
   }
 `;
 
 const StyledParagraphBottom = styled(P)`
   color: ${colors.TEXT_BOX};
-  padding-left: 15em;
+  padding-left: 20%;
   padding-right: 7em;
-  line-height: 1.8em;
+  line-height: 1.9em;
   padding-bottom: 5em;
 
   @media ${max.tablet} {
-    padding-left: 1em;
+    padding-left: 3em;
+    width: 100%;
   }
 `;
 
@@ -58,5 +79,6 @@ export {
   StyledHeading,
   StyledDirections,
   StyledBackground,
-  StyledParagraphBottom
+  StyledParagraphBottom,
+  StyledCodeOfConductContent
 };
