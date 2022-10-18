4b4b8c15d119454eef16e114a248f17b2448e78c
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-10-17 21:14:18 -0400

    responsiveness for desktop past projects

--- main-site/components/past-projects-section/PastProjectsSection.styles.ts
+++ main-site/components/past-projects-section/PastProjectsSection.styles.ts
@@ -1,27 +1,38 @@
 import styled from 'styled-components';
 import { colors } from '../../../shared-ui/style/colors';
 import { H2, P } from '../../../shared-ui/style/typography';
+import { max } from '../../../shared-ui/lib/responsive';
+
+const StyledPastProjects = styled.div``;
 
 const StyledPastProjectsHeader = styled(H2)`
-   {
-    color: ${colors.BLACK};
-    padding-bottom: 0.5em;
-    text-align: center;
-  }
+  color: ${colors.BLACK};
+  padding-bottom: 0.5em;
+  text-align: center;
 `;
 
 const StyledPastProjectsSection = styled.div`
-  padding-top: 10em;
   position: relative;
   background-color: ${colors.TEXT_BOX};
-  width: 70%;
+  width: 80%;
   margin: auto;
   border-radius: 20px;
-  height: 700px;
+  height: 100%;
+
+  @media ${max.tabletLg} {
+    margin-top: 6em;
+  }
 `;
 
 const StyledPastProjectsContainer = styled.div`
-  padding-left: 2em;
+  padding-top: 3em;
+  padding-left: 3em;
+`;
+
+const StyledPastProjectsContainerBottom = styled.div`
+  padding-top: 3em;
+  padding-left: 3em;
+  padding-bottom: 3em;
 `;
 
 const StyledPastProjectsInfo = styled.div`
@@ -29,40 +40,65 @@ const StyledPastProjectsInfo = styled.div`
   padding-left: 300px;
   padding-right: 3em;
 `;
+
+const StyledPastProjectsInfoContainer = styled.div`
+  text-align: center;
+`;
+
 const StyledPastProjectsTitle = styled(H2)`
   font-size: 25px;
   color: ${colors.BUTTON_GREEN};
-  padding-bottom: 0.7em;
+  padding-bottom: 0.9em;
 `;
 const StyledPastProjectsMembers = styled(P)`
   font-size: 13px;
   color: ${colors.WHITE};
-  padding-bottom: 0.7em;
+  padding-bottom: 0.9em;
 `;
 
 const StyledPastProjectsDescription = styled(P)`
   font-size: 18px;
   color: ${colors.WHITE};
-  padding-bottom: 0.7em;
+  padding-bottom: 0.9em;
+`;
+
+const StyledPastProjectsViewText = styled(P)`
+  font-size: 20px;
+  text-align: center;
+  padding-top: 3.5em;
+  padding-bottom: 2em;
+  line-height: 2;
+  color: ${colors.TEXT_BROWN};
 `;
 
 const StyledPastProjectsPhotos = styled.img`
   left: 0;
   float: left;
-  width: 300px;
-  height: 200px;
+  width: 320px;
+  height: 220px;
   padding-right: 1em;
 `;
 
 const StyledFennecFox = styled.img`
   position: absolute;
+  right: 0px;
+  padding-top: 46em;
+  z-index: 1;
+
+  @media ${max.tabletLg} {
+    display: none;
+  }
 `;
 
 const StyledCamel = styled.img`
   position: absolute;
-  left: 2em;
-  top: 3em;
-  width: 8em;
+  right: 0px;
+  z-index: 1;
+  padding-top: 10em;
+
+  @media ${max.tabletLg} {
+    display: none;
+  }
 `;
 export {
   StyledPastProjectsSection,
@@ -74,5 +110,9 @@ export {
   StyledPastProjectsMembers,
   StyledPastProjectsDescription,
   StyledPastProjectsPhotos,
-  StyledPastProjectsInfo
+  StyledPastProjectsInfo,
+  StyledPastProjectsViewText,
+  StyledPastProjectsInfoContainer,
+  StyledPastProjects,
+  StyledPastProjectsContainerBottom
 };
--- main-site/components/past-projects-section/PastProjectsSection.tsx
+++ main-site/components/past-projects-section/PastProjectsSection.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import FennecFox from '../../../shared-ui/images/fennec-fox.svg';
 import Inky from '../../../shared-ui/images/inky.jpg';
 import Swaple from '../../../shared-ui/images/swaple.png';
+import Camel from '../../../shared-ui/images/camel.svg';
+import Duck from '../../../shared-ui/images/duck.png';
 
 import {
   StyledPastProjectsSection,
@@ -12,12 +14,19 @@ import {
   StyledPastProjectsMembers,
   StyledPastProjectsDescription,
   StyledPastProjectsPhotos,
-  StyledPastProjectsInfo
+  StyledPastProjectsInfo,
+  StyledPastProjectsViewText,
+  StyledPastProjectsInfoContainer,
+  StyledCamel,
+  StyledPastProjects,
+  StyledPastProjectsContainerBottom
 } from './PastProjectsSection.styles';
 import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
 const PastProjectsSection: React.FC = () => {
   return (
-    <div>
+    <StyledPastProjects>
+      <StyledCamel src={Camel} />
+      <StyledFennecFox src={FennecFox} />
       <StyledPastProjectsHeader>Past Projects</StyledPastProjectsHeader>
       <StyledPastProjectsSection>
         <StyledPastProjectsContainer>
@@ -35,7 +44,8 @@ const PastProjectsSection: React.FC = () => {
             </StyledPastProjectsDescription>
             <PrimaryButton
               btnText="View Project"
-              btnLink="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
+              btnLink="https://devpost.com/software/inky-the-black-hole-pet
+              "
               newTab
             />
           </StyledPastProjectsInfo>
@@ -54,38 +64,43 @@ const PastProjectsSection: React.FC = () => {
             </StyledPastProjectsDescription>
             <PrimaryButton
               btnText="View Project"
-              btnLink="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
+              btnLink="https://brandonlim8890.github.io/wordle_variation/"
               newTab
             />
           </StyledPastProjectsInfo>
         </StyledPastProjectsContainer>
 
-        <StyledPastProjectsContainer>
-          <StyledPastProjectsPhotos src={Swaple} />
+        <StyledPastProjectsContainerBottom>
+          <StyledPastProjectsPhotos src={Duck} />
           <StyledPastProjectsInfo>
-            <StyledPastProjectsTitle>Swaple, 2022</StyledPastProjectsTitle>
-            <StyledPastProjectsMembers>
-              by Bob Zheng, Tze Zhe Brandon Lim, Nathan Kim, Daniel Ostapenko
-            </StyledPastProjectsMembers>
+            <StyledPastProjectsTitle>Oh Duck!, 2022</StyledPastProjectsTitle>
+            <StyledPastProjectsMembers>by Karyna Yen</StyledPastProjectsMembers>
             <StyledPastProjectsDescription>
-              A parody of wordle, but you swap letters from a list of words
-              instead! Try and complete the game in as few moves as possible.
+              'Oh, Duck!' is a chrome chrome extension that replaces profane
+              words with a duck emoji.
             </StyledPastProjectsDescription>
             <PrimaryButton
               btnText="View Project"
-              btnLink="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
+              btnLink="https://www.google.com/url?q=https://devpost.com/software/oh-duck&sa=D&source=editors&ust=1666054462827053&usg=AOvVaw1GNRfexvIUKuYxcp3Ey09w"
               newTab
             />
           </StyledPastProjectsInfo>
-        </StyledPastProjectsContainer>
+        </StyledPastProjectsContainerBottom>
       </StyledPastProjectsSection>
-      <StyledFennecFox src={FennecFox} />
-      <PrimaryButton
-        btnText="View Archive"
-        btnLink="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
-        newTab
-      />
-    </div>
+      <StyledPastProjectsInfoContainer>
+        <StyledPastProjectsViewText>
+          Interested in seeing more about our past events and past hacker
+          projects?
+          <br />
+          Check out the HackBeanpot Archive!
+        </StyledPastProjectsViewText>
+        <PrimaryButton
+          btnText="View Archive"
+          btnLink="https://projects.hackbeanpot.com/"
+          newTab
+        />
+      </StyledPastProjectsInfoContainer>
+    </StyledPastProjects>
   );
 };
 
