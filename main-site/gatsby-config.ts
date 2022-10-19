c2eef763e756897f82361e2698b6412332bcf9a5
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-18 23:56:58 -0400

    Added mailchimp signup.

--- main-site/gatsby-config.ts
+++ main-site/gatsby-config.ts
@@ -5,11 +5,19 @@ const config: GatsbyConfig = {
     title: `main-site`,
     siteUrl: `https://www.yourdomain.tld`
   },
+
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: []
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+          endpoint:  process.env.MAILCHIMP_ENDPOINT,
+          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
+  ]
 };
 
 export default config;
