36fe42f2430c98255a5f95a3b10cac207aa4eb1f
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-20 00:40:46 -0400

    Added styled mailgun signup button.

--- main-site/components/mailinglist-signup/MailinglistSignup.tsx
+++ main-site/components/mailinglist-signup/MailinglistSignup.tsx
@@ -1,57 +1,46 @@
 import React, { useState } from 'react';
 import addToMailChimp from 'gatsby-plugin-mailchimp';
+import { StyledEmailInput } from '../../../shared-ui/components/footer/Footer.styles';
+import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
 
-const MailinglistSignup = () => {
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-  
-    const handleSubmit = async (e: React.FormEvent) => {
-        console.log(email)
-      e.preventDefault();
-  
-      try {
-        const { result, msg } = await addToMailChimp(email);
-  
-        if (result !== 'success') {
-          throw msg; //error with subscription from our end or plugin
-        }
-  
+const MailinglistSignup: React.FunctionComponent<{}> = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    addToMailChimp(email)
+      .then((data: any) => {
         setEmail('');
         setMessage('Thank you for subscribing!');
-      } catch (error : any) {
-        if (error.includes('already subscribed')) {
-          setMessage("You're already subscribed!");
-        } else {
-          setMessage('Please enter a valid email.');
-        }
-      }
-    };
-  
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement> )=> {
-      e.preventDefault();
-      setEmail(e.target.value);
-    };
-  
-    return (
-      <div>
-        <form onSubmit={handleSubmit} className="footer__form">
-          <div className="footer__form-heading">Join our mailing list!</div>
-          <div className="footer__form-input-and-button">
-            <input
-              placeholder="Enter email here"
-              name="email"
-              type="text"
-              onChange={handleEmailChange}
-              className="footer__form-input"
-            />
-            <button className="footer__form-btn" type="submit">
-              Join
-            </button>
-          </div>
-          <div className="footer__form-msg">{message}</div>
-        </form>
-      </div>
-    );
+      })
+      .catch((error: Error) => {
+        setMessage('There was an error. Please try again');
+      });
+
   };
-  
-  export default MailinglistSignup;
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="footer__form">
+        <StyledEmailInput
+          placeholder="Enter your email"
+          name="email"
+          type="text"
+          onChange={handleEmailChange} />
+        <PrimaryButton
+          btnText="Join our mailing list" btnLink={''} type="submit"        />
+      <div className="footer__form-msg">{message}</div>
+    </form>
+    </div>
+  );
+
+};
+
+export default MailinglistSignup;
