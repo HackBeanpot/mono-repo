50c0fecc64f90baf880bef3b25c3736cb9b95ceb
mike10911 <mikeymundia@gmail.com>
2022-10-22 01:31:14 -0400

    removes code duplication and build fix

--- main-site/components/testimonials-section/testimonial-card/LeftOrRightTestimonialCard.tsx
+++ main-site/components/testimonials-section/testimonial-card/LeftOrRightTestimonialCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getImage } from '../../../lib/utils';
 import {
   StyledTestimonialCardContainer,
   StyledTestimonialCardBox,
@@ -7,25 +8,6 @@ import {
   StyledTestimonialCardQuote,
   StyledTestimonialCardYear
 } from './TestimonialCard.styles';
-
-import Karyna from '../../../../shared-ui/images/karyna.png';
-import Jimin from '../../../../shared-ui/images/jimin.png';
-import Spencer from '../../../../shared-ui/images/spencer.png';
-import Group from '../../../../shared-ui/images/group.png';
-import { LeftOrRightTestimonialCardProps } from '../../../lib/types';
-
-function getImage(author: string): string {
-  if (author === 'Karyna Yen') {
-    return Karyna;
-  }
-  if (author === 'Ji-min Kim') {
-    return Jimin;
-  }
-  if (author === 'Spencer Shao') {
-    return Spencer;
-  }
-  return Group;
-}
 
 const LeftOrRightTestimonialCard: React.FC<LeftOrRightTestimonialCardProps> = ({
   testimonial
