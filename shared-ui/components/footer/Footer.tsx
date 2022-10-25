09951401a0cad579757ce7a0a494ab416d401aae
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-25 00:00:45 -0400

    Footer changes.

--- shared-ui/components/footer/Footer.tsx
+++ shared-ui/components/footer/Footer.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FooterProps, TabInfo } from '../../lib/types';
 import {
   StyledButtonContainer,
   StyledButtonRow,
-  StyledEmailInput,
   StyledFooter,
   StyledFooterText,
   StyledFooterTextLink,
   StyledHackBeanpotLogo,
-  StyledSocialMediaButtons,
   StyledTab,
   StyledTabContainer,
   StyledFooterContentContainer,
@@ -16,16 +14,9 @@ import {
   StyledHBPContent
 } from './Footer.styles';
 import HackBeanpotWhiteLogo from '../../images/hackbeanpot-logo-white.svg';
-import Instagram from '../../images/social-icons/instagram.svg';
-import Facebook from '../../images/social-icons/facebook.svg';
-import Medium from '../../images/social-icons/medium.svg';
-import Twitter from '../../images/social-icons/twitter.svg';
-import PrimaryButton from '../primary-button/PrimaryButton';
-import MailinglistSignup from '../../../main-site/components/mailinglist-signup/MailinglistSignup';
+import MailingListSignup from '../../../main-site/components/mailing-list-signup/MailingListSignup';
 
 const Footer: React.FC<FooterProps> = ({ tabs, isDay }) => {
-  const [email, setEmail] = useState<string>('');
-
   return (
     <StyledFooter isDay={isDay}>
       <StyledHBPContent>
@@ -52,29 +43,8 @@ const Footer: React.FC<FooterProps> = ({ tabs, isDay }) => {
         </StyledTabContainer>
 
         <StyledButtonContainer>
-          <StyledEmailInput
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e: React.FormEvent<HTMLInputElement>): void =>
-              setEmail(e.currentTarget.value)
-            }
-          />
           <StyledButtonRow>
-            <MailinglistSignup/>
-            <StyledSocialMediaButtons>
-              <a href="https://www.instagram.com/hackbeanpot/" target="_blank">
-                <img width="30" height="30" src={Instagram} />
-              </a>
-              <a href="https://www.facebook.com/hackbeanpot" target="_blank">
-                <img width="30" height="30" src={Facebook} />
-              </a>
-              <a href="https://hackbeanpot.medium.com/" target="_blank">
-                <img width="30" height="30" src={Medium} />
-              </a>
-              <a href="https://twitter.com/HackBeanpot" target="_blank">
-                <img width="30" height="30" src={Twitter} />
-              </a>
-            </StyledSocialMediaButtons>
+            <MailingListSignup />
           </StyledButtonRow>
         </StyledButtonContainer>
       </StyledFooterContentContainer>
