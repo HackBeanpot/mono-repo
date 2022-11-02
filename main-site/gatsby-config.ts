4b26d38c90ec84b836b6147ef2edaa01eda031ba
Dhivas-Sugumar <65351416+Dhivas-Sugumar@users.noreply.github.com>
2022-11-01 22:34:45 -0400

    Mailchimp signup (#44)
    * Added mailchimp signup.
    
    * Added component to signup.
    
    * Added mailing signup component to footer.
    
    * Added styled mailgun signup button.
    
    * Added styling.
    
    * Fixed the stylng for the mailing list signup.
    
    * Fixed styling for mailing signup.
    
    * Fixed all styling.
    
    * FIxed types.
    
    * MIssing commit added.
    
    * Footer changes.
    
    * Fixed build error.
    
    * FIxed build errors.
    
    * freaking enpoint was wrong :(
    
    * removed console log.
    
    * fixed build error oops.

--- main-site/gatsby-config.ts
+++ main-site/gatsby-config.ts
@@ -5,11 +5,20 @@ const config: GatsbyConfig = {
     title: `main-site`,
     siteUrl: `https://www.yourdomain.tld`
   },
+
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: []
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint:
+          'https://hackbeanpot.us10.list-manage.com/subscribe/post?u=a98050d47fdae2481521f0474&id=dccd8c8431&f_id=00e336e2f0'
+      }
+    }
+  ]
 };
 
 export default config;
