65fd6edc8fb324d29bf3eec3495c3f5a4f3c8ece
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2022-11-02 22:30:35 -0400

    hehe

--- main-site/src/pages/404.tsx
+++ main-site/src/pages/404.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
 import { min } from '../../../shared-ui/lib/responsive';
 import useMatchMedia from 'react-use-match-media';
-import '../page-styles/globals.css';
+import '../pageStyles/globals.css';
 import { StyledH1, StyledH4, ButtonContainer} from '../page-styles/404.styles';
 
 const NotFoundPage: React.FC = () => {
