fa03eeac85e008879495ceb3709042ea7677e919
jzhzhang66 <jzhzhang66@gmail.com>
2022-11-06 17:03:17 -0500

    test moving styled components into page file

--- main-site/src/pages/codeOfConduct.tsx
+++ main-site/src/pages/codeOfConduct.tsx
@@ -5,15 +5,61 @@ import '../../../shared-ui/style/global.css';
 import '../pageStyles/globals.css';
 import '../../../shared-ui/index.css';
 import Header from '../../../shared-ui/components/header/Header';
-import {
-  StyledParagraph,
-  StyledHeading,
-  StyledDirections,
-  StyledBackground,
-  StyledParagraphBottom,
-  StyledTextContainer,
-  StyledBodyTextContainer
-} from '../page-styles/code-of-conduct.styles';
+import styled from 'styled-components';
+import { colors } from '../../../shared-ui/style/colors';
+import { H2, P } from '../../../shared-ui/style/typography';
+import { max } from '../../../shared-ui/lib/responsive';
+// import {
+//   StyledParagraph,
+//   StyledHeading,
+//   StyledDirections,
+//   StyledBackground,
+//   StyledParagraphBottom,
+//   StyledTextContainer,
+//   StyledBodyTextContainer
+// } from '../page-styles/code-of-conduct.styles';
+
+const StyledTextContainer = styled.div`
+  margin: 0 6em;
+  padding: 8em 0;
+
+  @media ${max.tablet} {
+    margin: 0 2em;
+  }
+`;
+
+const StyledBodyTextContainer = styled.div`
+  padding-left: 5em;
+
+  @media ${max.tablet} {
+    padding-left: 0;
+  }
+`;
+
+const StyledBackground = styled.div`
+  background-color: ${colors.DUNE_PRIMARY};
+`;
+
+const StyledParagraph = styled(P)`
+  color: ${colors.TEXT_BOX};
+  line-height: 1.8em;
+  padding-bottom: 1em;
+`;
+
+const StyledHeading = styled(H2)`
+  padding-bottom: 1.5em;
+  color: ${colors.TEXT_BOX};
+`;
+const StyledDirections = styled(P)`
+  padding-top: 2em;
+  color: ${colors.TEXT_BOX};
+  font-size: 1.875em;
+`;
+
+const StyledParagraphBottom = styled(P)`
+  color: ${colors.TEXT_BOX};
+  line-height: 1.9em;
+`;
 
 const CodeOfConduct: React.FC = () => {
   return (
