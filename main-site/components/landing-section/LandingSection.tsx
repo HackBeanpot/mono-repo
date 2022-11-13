c270a8c3d127a656bad7ec5a0f2bb02fabbc4878
mike10911 <mikeymundia@gmail.com>
2022-11-12 23:01:17 -0500

    updates

--- main-site/components/landing-section/LandingSection.tsx
+++ main-site/components/landing-section/LandingSection.tsx
@@ -12,30 +12,36 @@ import Toggle from '../../../shared-ui/images/toggle-day-dark.svg';
 import Sun from '../../../shared-ui/images/sun.svg';
 import Moon from '../../../shared-ui/images/moon.svg';
 import { LandingSectionProps } from '../../lib/types';
-import { emojiRotate, fadeInUp } from './LandingSectionAnimations';
+import { emojiRotate, fadeInUp, stagger } from './LandingSectionAnimations';
 
 const LandingSection: React.FC<LandingSectionProps> = ({ isDay, setIsDay }) => {
   return (
-    <StyledLandingSectionContainer  initial='initial' animate='animate'>
+    <StyledLandingSectionContainer initial="initial" animate="animate">
       <StyledToggle
         src={Toggle}
         alt="toggle-day-night"
         onClick={(): void => setIsDay(!isDay)}
       />
-
       {isDay ? <StyledStar src={Sun} /> : <StyledStar src={Moon} />}
-      <StyledLandingTextContainer variants={fadeInUp}>
-        <StyledHackathonText >
+      <StyledLandingTextContainer variants={stagger}>
+        <StyledHackathonText variants={fadeInUp}>
           Tech Team 2023
         </StyledHackathonText>
-        <StyledThemeText>
+        <StyledThemeText variants={fadeInUp}>
           Animations Workshop
         </StyledThemeText>
-        <StyledHackathonText>
+        <StyledHackathonText variants={fadeInUp}>
           Framer Motion <br />
           By Mike
         </StyledHackathonText>
-        <StyledEmoji variants={emojiRotate}>🤪</StyledEmoji>
+        <StyledEmoji
+          whileHover={{ scale: 1.5 }}
+          drag
+          dragConstraints={{ left: -800, right: 800, top: -600, bottom: 5000 }}
+          variants={emojiRotate}
+        >
+          🤪
+        </StyledEmoji>
       </StyledLandingTextContainer>
     </StyledLandingSectionContainer>
   );
--- main-site/components/landing-section/LandingSectionAnimations.tsx
+++ main-site/components/landing-section/LandingSectionAnimations.tsx
@@ -1,20 +1,30 @@
 export const fadeInUp = {
-    initial: {
-      y: 60,
-      opacity: 0
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 1
-      }
+  initial: {
+    y: 60,
+    opacity: 0
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1
     }
-  };
+  }
+};
 
-export const emojiRotate ={
-    animate : { rotate : [0, 200, -200, 360, 0] , x : [-800,800,-800,800,-800] ,y: [400, -400, 400],
-    transition : { repeat: Infinity, duration : 5}},
+export const emojiRotate = {
+  animate: {
+    rotate: [0, 200, -200, 360, 0],
+    x: [-800, 800, -800, 800, -800],
+    y: [400, -400, 400],
+    transition: { repeat: Infinity, duration: 10 }
+  }
+};
 
-}  
-
+export const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.5
+    }
+  }
+};
