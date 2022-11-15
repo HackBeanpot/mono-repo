aee8d7ad2baf68d7bed82fe948acc991624fe13f
surbhi <surbhigulati2000@gmail.com>
2022-11-15 17:37:08 -0500

    clean up css

--- shared-ui/components/meet-the-team/meet-all-core/MeetAllCore.styles.ts
+++ shared-ui/components/meet-the-team/meet-all-core/MeetAllCore.styles.ts
@@ -1,73 +1,37 @@
 import styled from 'styled-components';
-import { H3, P } from '../../../style/typography';
-import { min } from '../../../../shared-ui/lib/responsive';
+import { P } from '../../../style/typography';
+import { min } from '../../../lib/responsive';
+import { colors } from '../../../style/colors';
 
 const StyledMeetCoreContainer = styled.div`
   display: flex;
   position: relative;
   margin: 5em 0;
   padding-bottom: 5em;
+  text-align: left;
   @media ${min.tablet} {
     padding: 10em 0;
   }
 `;
 
+const StyledOurTeamCore = styled(P)`
+  color: ${colors.WHITE};
+  padding-top: 3.5em;
+`;
+
 const StyledDesktopImageCore = styled.img`
-  display: flex;
+  right: 5em;
   padding-top: 3.5em;
-  justify-content: center;
-`;
-
-const StyledDesktopOurTeamCore = styled(P)`
-  display: flex;
-  padding-top: 3.5em;
-  justify-content: center;
-`;
-
-const StyledDesktopHeaderCore = styled(H3)`
-  display: flex;
-  padding-top: 3.5em;
-  justify-content: center;
-`;
-
-const StyledDesktopParagraphCore = styled(P)`
-  display: flex;
-  padding-top: 3.5em;
-  justify-content: center;
 `;
 
 const StyledMobileImageCore = styled.img`
-  display: flex;
+  right: 3em;
   padding-top: 3.5em;
-  justify-content: center;
-`;
-
-const StyledMobileOurTeamCore = styled(P)`
-  display: flex;
-  padding-top: 3.5em;
-  justify-content: center;
-`;
-
-const StyledMobileHeaderCore = styled(H3)`
-  display: flex;
-  padding-top: 3.5em;
-  justify-content: center;
-`;
-
-const StyledMobileParagraphCore = styled(P)`
-  display: flex;
-  padding-top: 3.5em;
-  justify-content: center;
 `;
 
 export {
   StyledMeetCoreContainer,
+  StyledOurTeamCore,
   StyledDesktopImageCore,
-  StyledDesktopOurTeamCore,
-  StyledDesktopHeaderCore,
-  StyledDesktopParagraphCore,
-  StyledMobileImageCore,
-  StyledMobileOurTeamCore,
-  StyledMobileHeaderCore,
-  StyledMobileParagraphCore
+  StyledMobileImageCore
 };
--- shared-ui/components/meet-the-team/meet-all-core/MeetAllCore.tsx
+++ shared-ui/components/meet-the-team/meet-all-core/MeetAllCore.tsx
@@ -2,52 +2,45 @@ import React from 'react';
 import { min } from '../../../lib/responsive';
 import useMatchMedia from 'react-use-match-media';
 import {
+  StyledMeetCoreContainer,
+  StyledOurTeamCore,
   StyledDesktopImageCore,
-  StyledDesktopOurTeamCore,
-  StyledDesktopHeaderCore,
-  StyledDesktopParagraphCore,
-  StyledMobileImageCore,
-  StyledMobileOurTeamCore,
-  StyledMobileHeaderCore,
-  StyledMobileParagraphCore
+  StyledMobileImageCore
 } from './MeetAllCore.styles';
 import GroupDesktop from '../../../images/meet-the-team/groupDesktop.png';
 import GroupMobile from '../../../images/meet-the-team/groupMobile.png';
+import { H3, P } from '../../../style/typography';
 
 const MeetAllCore: React.FC = () => {
   const isDesktop = useMatchMedia(min.tablet);
   return (
     <div id="meetAllCore">
       {isDesktop && (
-        <div>
-          <StyledDesktopOurTeamCore>OUR TEAM</StyledDesktopOurTeamCore>
-          <StyledDesktopHeaderCore>
-            Meet this year’s pioneers behind HackBeanpot!
-          </StyledDesktopHeaderCore>
-          <StyledDesktopParagraphCore>
+        <StyledMeetCoreContainer>
+          <StyledOurTeamCore>OUR TEAM</StyledOurTeamCore>
+          <H3>Meet this year’s pioneers behind HackBeanpot!</H3>
+          <P>
             The HackBeanpot Core Team has been hard at work to create the best
             adventure for you at HackBeanpot 2023! We are a group of 23 driven
             students from universities in the Boston-area who aim to make
             technical experience and knowledge accessible to anyone who is
             interested!
-          </StyledDesktopParagraphCore>
+          </P>
           <StyledDesktopImageCore src={GroupDesktop} />
-        </div>
+        </StyledMeetCoreContainer>
       )}
       {!isDesktop && (
         <div>
-          <StyledMobileOurTeamCore>OUR TEAM</StyledMobileOurTeamCore>
-          <StyledMobileHeaderCore>
-            Meet this year’s pioneers behind HackBeanpot!
-          </StyledMobileHeaderCore>
+          <StyledOurTeamCore>OUR TEAM</StyledOurTeamCore>
+          <H3>Meet this year’s pioneers behind HackBeanpot!</H3>
           <StyledMobileImageCore src={GroupMobile} />
-          <StyledMobileParagraphCore>
+          <P>
             The HackBeanpot Core Team has been hard at work to create the best
             adventure for you at HackBeanpot 2023! We are a group of 23 driven
             students from universities in the Boston-area who aim to make
             technical experience and knowledge accessible to anyone who is
             interested!
-          </StyledMobileParagraphCore>
+          </P>
         </div>
       )}
     </div>
