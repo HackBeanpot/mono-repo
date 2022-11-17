2198399a001e3f56141593b023e37c7603830050
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-17 14:57:45 -0500

    added button and styling

--- shared-ui/components/time-remaining/TimeRemaining.styles.tsx
+++ shared-ui/components/time-remaining/TimeRemaining.styles.tsx
@@ -2,6 +2,11 @@ import styled from "styled-components";
 import { colors } from "../../style/colors";
 import { fonts, P } from "../../style/typography";
 
+const StyledTimeRemainingContainer = styled.div`
+    text-align: center;
+
+`;
+
 const StyledTimeRemaing = styled.div`
   box-sizing: border-box;
   display: flex;
@@ -12,14 +17,20 @@ const StyledTimeRemaing = styled.div`
 
 `;
 
-
 const StyledTimeRemainingHeader = styled(P)`{
     color: ${colors.TEXT_BROWN};
     font-family: ${fonts.nunitoSansLight};
     font-size: 2em;
-    text-align: center;
     padding: 0.5em;
 }
 `;
 
-export { StyledTimeRemaing, StyledTimeRemainingHeader };
+
+const StyledButtonContainer = styled.div`
+  margin-top: 0.5em;
+  justify-content: center;
+
+ 
+`;
+
+export { StyledTimeRemaing, StyledTimeRemainingHeader, StyledButtonContainer,StyledTimeRemainingContainer };
--- shared-ui/components/time-remaining/TimeRemaining.tsx
+++ shared-ui/components/time-remaining/TimeRemaining.tsx
@@ -1,20 +1,23 @@
 import React from "react"
-import { StyledTimeRemainingHeader } from "./TimeRemaining.styles"
+import PrimaryButton from "../primary-button/PrimaryButton";
+import { StyledTimeRemainingHeader, StyledButtonContainer, StyledTimeRemainingContainer } from "./TimeRemaining.styles"
 
 
 
 
 const TimeRemaining = () => {
-
-
-
-
-
     return (
-        <StyledTimeRemainingHeader>Time Remaining</StyledTimeRemainingHeader>
+        <StyledTimeRemainingContainer>
 
-
-
+            <StyledTimeRemainingHeader>Time Remaining</StyledTimeRemainingHeader>
+            <StyledButtonContainer>
+                <PrimaryButton
+                    btnText="Share with friends!"
+                    btnLink="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
+                    newTab
+                />
+            </StyledButtonContainer>
+        </StyledTimeRemainingContainer>
 
     )
 
