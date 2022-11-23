dd49d963c21e5032992981418edf22342dab3d09
Judy Zhang <59738880+jzhzhang66@users.noreply.github.com>
2022-11-23 09:36:22 -0500

    FIX COC 4 real (#73)
    * works like dis
    
    * move 404 to shared ui
    
    * moved everything to one globals.css file
    
    * update globals path
    
    * added 404 page to live site
    
    * import globals css in code of conduct
    
    * redirect user from code of conduct to main site
    
    * added in link to redirect to code of conduct
    
    * added styled components gatsby plugin to live site

---
+++ shared-ui/components/404/404.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
+import { min } from '../../../shared-ui/lib/responsive';
+import useMatchMedia from 'react-use-match-media';
+import '../../style/globals.css';
+import { StyledH1, StyledH4, ButtonContainer } from './404.styles';
+
+const NotFound: React.FC = () => {
+  const isDesktop = useMatchMedia(min.tablet);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const getBackgroundClassName = (): string => {
+    if (isDesktop) {
+      return 'error-background-desktop';
+    }
+    return 'error-background-mobile';
+  };
+  React.useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <div className={getBackgroundClassName()}>
+      <StyledH1>
+        Uh oh, how did we end <br /> up here?
+      </StyledH1>
+      <StyledH4>Let's get back to exploring!</StyledH4>
+      <ButtonContainer>
+        <PrimaryButton btnText="return home" btnLink="/"></PrimaryButton>
+      </ButtonContainer>
+    </div>
+  );
+};
+
+export default NotFound;
