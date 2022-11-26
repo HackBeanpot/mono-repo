7de4910716b2b4f20c1eefbc6fa4d60222385e70
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-11-26 01:13:48 -0500

    Fixed frontend issues.

--- live-site/components/hacking-remaning/HackingRemaining.styles.tsx
+++ live-site/components/hacking-remaning/HackingRemaining.styles.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
+import { P } from '../../../shared-ui/lib/typography';
 import { colors } from '../../../shared-ui/style/colors';
 
 const StyledHackingRemainingContainer = styled.div`
   z-index: 21;
-  float: right;
   background-color: ${colors.BUTTON_DARK_GREEN};
+  position: fixed;
+  right: 0;
+  top: 4em;
 
-  border-radius: 1em;
+  width: 133px;
+  height: 139px;
+
+  text-align: center;
+
+  border-radius: 1em 0em 0em 1em;
   border: 0.3em solid ${colors.BUTTON_DARK_GREEN};
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
     drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
@@ -14,7 +22,7 @@ const StyledHackingRemainingContainer = styled.div`
   margin: 2em 0em;
 `;
 
-const StyledHackingRemainingHeader = styled.p`
+const StyledHackingRemainingHeader = styled(P)`
   font-size: 0.9375em;
   color: ${colors.BUTTON_GREEN};
 `;
