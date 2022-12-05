35c0bee409c44b506b36e55b6b8a22f0495d35bd
jzhzhang66 <jzhzhang66@gmail.com>
2022-12-04 23:03:59 -0500

    add types

--- live-site/src/hooks/useAirtable.ts
+++ live-site/src/hooks/useAirtable.ts
@@ -2,7 +2,15 @@ import { useState, useEffect } from 'react';
 
 const BASEURL = 'https://airtable.hackbeanpot.com';
 
-export const useAirtableApi = (baseName: string, tableName: string) => {
+interface AirtableData {
+  data: never[];
+  isLoading: boolean;
+}
+
+export const useAirtableApi = (
+  baseName: string,
+  tableName: string
+): AirtableData => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   console.log(data);
@@ -27,7 +35,10 @@ export const useAirtableApi = (baseName: string, tableName: string) => {
   return { data, isLoading };
 };
 
-export const useAirtableApiWithPagination = (baseName, tableName) => {
+export const useAirtableApiWithPagination = (
+  baseName: string,
+  tableName: string
+): AirtableData => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -35,11 +46,11 @@ export const useAirtableApiWithPagination = (baseName, tableName) => {
     const url = `${BASEURL}/${baseName}/${tableName}`;
     let offset = '';
     const records = [];
-    const getData = async () => {
+    const getData = async (): Promise<void> => {
       do {
         const urlWithOffset = new URL(url);
         urlWithOffset.searchParams.append('offset', offset);
-        const responseData = await fetch(urlWithOffset);
+        const responseData = await fetch(urlWithOffset.toString());
         const responseJson = await responseData.json();
         records.push(...responseJson.records);
         offset = responseJson.offset;
