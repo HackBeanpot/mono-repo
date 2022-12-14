ff24cabe2625ae34fe161d1f6dc33ad90a0b5b68
surbhi <surbhigulati2000@gmail.com>
2022-12-14 18:48:44 -0500

    add StyledJudgingSelection

--- live-site/components/judging-section/judging-selection/JudgingSelection.styles.tsx
+++ live-site/components/judging-section/judging-selection/JudgingSelection.styles.tsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import { H2 } from '../../../../shared-ui/style/typography';
 
-const StyledSelection = styled(H2)`
+const StyledJudgingSelection = styled.div`
+  padding: 10em;
+`;
+
+const StyledWhoAreYou = styled(H2)`
   text-align: center;
 `;
 
 export { 
-  StyledSelection
+  StyledJudgingSelection,
+  StyledWhoAreYou
 }
--- live-site/components/judging-section/judging-selection/JudgingSelection.tsx
+++ live-site/components/judging-section/judging-selection/JudgingSelection.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import SecondaryButton from '../../../../shared-ui/components/secondary-button/SecondaryButton';
-import { StyledSelection } from './JudgingSelection.styles';
+import { StyledJudgingSelection, StyledWhoAreYou } from './JudgingSelection.styles';
 
 const JudgingSelection: React.FC = () => {
   
   return (
-    <div>
-      <StyledSelection> Who are you? </StyledSelection>
+    <StyledJudgingSelection>
+      <StyledWhoAreYou> Who are you? </StyledWhoAreYou>
       <span>
         <SecondaryButton btnText='I am a judge' btnLink='judges' />
         <SecondaryButton btnText='I am a hacker' btnLink='hackers' />
       </span>
-    </div>
+    </StyledJudgingSelection>
   );
 };
 
