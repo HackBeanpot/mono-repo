c21dac1895b3bf09558f03888ea00079c673678e
Mike <103906123+mike10911@users.noreply.github.com>
2022-12-14 00:17:32 -0500

    Live Event Schedule (#97)
    * finished event schedule
    
    * bottom border tab fix
    
    * fixes
    
    * fixed spelling and emtpy styles

---
+++ live-site/components/event-schedule-section/EventScheduleSection.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
+import { mockData } from './EventScheduleMockData';
+
+import {
+  StyledBoldSubText,
+  EventScheduleSectionContainer,
+  StyledEventScheduleHeader,
+  StyledEventScheduleSubText,
+  ButtonContainer,
+  EventScheduleTextButtonContainer
+} from './EventScheduleSection.styles';
+import EventScheduleTab from './EventScheduleTab';
+
+const EventScheduleSection: React.FC = () => {
+  return (
+    <EventScheduleSectionContainer>
+      <StyledEventScheduleHeader>Event Schedule</StyledEventScheduleHeader>
+      <EventScheduleTextButtonContainer>
+        <StyledEventScheduleSubText>
+          Dates and times are displayed in your local timezone. Schedule in EST
+          can be found here.
+          <br />
+          The password for all Zoom meetings can be found pinned in the
+          #announcements Slack channel.
+          <br />
+          <StyledBoldSubText>
+            Please be sure to attend all events labeled "Everyone".
+          </StyledBoldSubText>
+        </StyledEventScheduleSubText>
+        <ButtonContainer>
+          <PrimaryButton
+            btnText="Add to Calendar +"
+            btnLink=""
+          ></PrimaryButton>
+        </ButtonContainer>
+      </EventScheduleTextButtonContainer>
+      <EventScheduleTab tabs={mockData}></EventScheduleTab>
+    </EventScheduleSectionContainer>
+  );
+};
+
+export default EventScheduleSection;
