f30f5cfd7841a91f47ee8db7a33435402165aa63
surbhi <surbhigulati2000@gmail.com>
2022-12-14 03:16:31 -0500

    see if link backward works now

--- live-site/components/judging-section/judging-schedule-hackers/JudgingScheduleHackers.tsx
+++ live-site/components/judging-section/judging-schedule-hackers/JudgingScheduleHackers.tsx
@@ -11,7 +11,7 @@ const JudgingScheduleHackers: React.FC = () => {
 
   return (
     <div>
-      <PrimaryButton btnText='back' btnLink='www.google.com' />
+      <PrimaryButton btnText='back' btnLink='../' />
       <StyledWelcomePerson> Welcome, Hacker! </StyledWelcomePerson>
       <StyledSecondaryDropdown options={options} />
     </div>
