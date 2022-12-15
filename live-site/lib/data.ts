1fcc7e48fad9b08c7ad500abd11528dfa0d5eb93
surbhi <surbhigulati2000@gmail.com>
2022-12-14 20:42:08 -0500

    see if links fix with ./

--- live-site/lib/data.ts
+++ live-site/lib/data.ts
@@ -1,12 +1,12 @@
 import { TabInfo } from './types';
 
 const liveSiteTabInfo: TabInfo[] = [
-  { name: 'Schedule', link: '#schedule' },
-  { name: 'Mentors', link: '#mentors' },
-  { name: 'Challenges', link: '#challenges' },
-  { name: 'Resources', link: '#resources' },
-  { name: 'Team', link: '#team' },
-  { name: 'Judging', link: 'judging' }
+  { name: 'Schedule', link: './#schedule' },
+  { name: 'Mentors', link: './#mentors' },
+  { name: 'Challenges', link: './#challenges' },
+  { name: 'Resources', link: './#resources' },
+  { name: 'Team', link: './#team' },
+  { name: 'Judging', link: './judging' }
 ];
 
 interface TimeLeft {
