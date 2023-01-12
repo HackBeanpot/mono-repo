d610b7709f33d6ae609d9deddecb3b74fe9dfa79
surbhi <surbhigulati2000@gmail.com>
2023-01-11 22:45:48 -0500

    revamp the sortJudgesAndPeople method

--- judging-algorithm/formSchedule.ts
+++ judging-algorithm/formSchedule.ts
@@ -1,41 +1,34 @@
 import { HackerTeam, Judge, Room } from "./types";
+import * as _ from "lodash";
 
 // driver code, takes parsed values and creates random assignments 
 function sortJudgesAndPeople(allTimes: string[], allJudgingRooms: Room[], unassignedTeams: HackerTeam[], unassignedJudges: Judge[]) {
-
   // judges assignment
-  judgesPerRoom: int = unassignedJudges.length / allJudgingRooms.length;
-  roomIdx: int = -1;
-  while (unassignedJudges.length > 0) {
-    roomIdx++;
-    judgesInCurRoom: int = 0;
-    while (judgesInCurRoom < judgesPerRoom) {
-      judgeToAssignIdx = Math.floor(Math.random() * unassignedJudges.length + 1);
-      judgeToAssign = unassignedJudges.pop(judgeToAssignIdx);
-      judgesInCurRoom++;
-
-      assignJudgeToRoom(judgeToAssign, roomIdx);
+  let judgesPerRoom = unassignedJudges.length / allJudgingRooms.length;
+  for (let roomIdx = 0; roomIdx < allJudgingRooms.length; roomIdx++) {
+    for (let judgeCount = 0; judgeCount < judgesPerRoom; judgeCount++) {
+      if (unassignedJudges.length > 0) {
+        let judgeToAssignIdx = Math.floor(Math.random() * unassignedJudges.length + 1);
+        let judgeToAssign = unassignedJudges.splice(judgeToAssignIdx, 1).at(0);
+        assignJudgeToRoom(judgeToAssign, roomIdx);
+      }
     }
   }
 
   // people assignment
+  let curTimeSlotIdx = 0;
   while (unassignedTeams.length > 0) {
-    const timeSlot: string = getNextTime();
-    availableRooms = this.deepArrayCopy(allJudgingRooms);
-    while (availableRooms.length() > 0 && unassignedTeams.length > 0) {
-      const teamToAssignIdx = Math.floor(Math.random() * unassignedTeams.length + 1);
-      const teamToAssign = unassignedTeams.pop(teamToAssignIdx);
-
-      const roomToAssignToIdx = Math.floor(Math.random() * availableRooms.length() + 1);
-      const roomToAssignTo = availableRooms.pop(roomToAssignToIdx);
-
-      assignTeamToTime(timeSlot, teamsToAssign, roomToAssignTo);
+    const timeSlot = allTimes.at(curTimeSlotIdx);
+    for (let roomInTimeSlot = 0; roomInTimeSlot < allJudgingRooms.length; roomInTimeSlot++) {
+      if (unassignedTeams.length == 0) {
+        break;
+      }
+      const teamToAssign = unassignedTeams.pop();
+      const roomToAssignTo = allJudgingRooms.at(roomInTimeSlot);
+      assignTeamToTime(timeSlot, teamToAssign, roomToAssignTo);
     }
+    curTimeSlotIdx++;
   }
-}
-
-function getNextTime() {
-  return allTimes.pop();
 }
 
 assignTeamToTime(timeSlot: string, teamToAssign: Team, roomToAssignTo: Room) {
