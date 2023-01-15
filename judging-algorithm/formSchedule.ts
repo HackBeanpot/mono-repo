886abff9a0ff4cb59da6fad6c0c193877c5fa3bf
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2023-01-15 16:23:31 -0500

    fixed eslint errors

--- judging-algorithm/formSchedule.ts
+++ judging-algorithm/formSchedule.ts
@@ -1,26 +1,46 @@
-import { FinalOutputTables, HackerOutput, HackerTeam, Judge, JudgeOutput, Room } from "./types";
+import {
+  FinalOutputTables,
+  HackerOutput,
+  HackerTeam,
+  JudgeOutput
+} from './types';
 
-// driver code, takes parsed values and creates random assignments 
+// driver code, takes parsed values and creates random assignments
 // the judges and hacker teams it take in have already been filtered, and rooms have been filtered for capacity
-export function sortJudgesAndPeople(allTimes: string[], allJudgingRooms: string[], unassignedTeams: HackerTeam[], unassignedJudges: string[]): FinalOutputTables {
-  const judgesTable: JudgeOutput[] = []
-  const hackerTable: HackerOutput[] = []
-  const hashMap = new Map<string, JudgeOutput[]>()
-  
+export function sortJudgesAndPeople(
+  allTimes: string[],
+  allJudgingRooms: string[],
+  unassignedTeams: HackerTeam[],
+  unassignedJudges: string[]
+): FinalOutputTables {
+  const judgesTable: JudgeOutput[] = [];
+  const hackerTable: HackerOutput[] = [];
+  const roomsToJudgeOutputs = new Map<string, JudgeOutput[]>();
+  const finalOutput: FinalOutputTables = {
+    judgeOutput: judgesTable,
+    hackerOutput: hackerTable
+  };
   // judges assignment
-  const judgesPerRoom = unassignedJudges.length / allJudgingRooms.length;
-  for (let roomIdx = 0; roomIdx < allJudgingRooms.length; roomIdx++) {
+  const judgesPerRoom = Math.ceil(
+    unassignedJudges.length / allJudgingRooms.length
+  );
+  for (const room of allJudgingRooms) {
     for (let judgeCount = 0; judgeCount < judgesPerRoom; judgeCount++) {
       if (unassignedJudges.length > 0) {
-        const room: string = allJudgingRooms[roomIdx]
-        let judgeToAssignIdx = Math.floor(Math.random() * unassignedJudges.length + 1);
-        let judgeToAssign = unassignedJudges.splice(judgeToAssignIdx, 1).at(0)!;
-        const newJudge: JudgeOutput = assignJudgeToRoom(judgeToAssign, room);
-        judgesTable.push(newJudge) // we mutate this later
-        if (hashMap.has(room)) {
-          hashMap.set(room, hashMap.get(room).push(newJudge))
+        const judgeToAssignIdx = Math.floor(
+          Math.random() * unassignedJudges.length + 1
+        );
+        const newJudge: JudgeOutput = assignJudgeToRoom(
+          unassignedJudges[judgeToAssignIdx],
+          room
+        );
+        unassignedJudges.splice(judgeToAssignIdx, 1);
+        judgesTable.push(newJudge); // we mutate this later
+        // add to the mapping of room number to judges
+        if (roomsToJudgeOutputs.has(room)) {
+          roomsToJudgeOutputs.get(room).push(newJudge);
         } else {
-          hashMap.set(room, [newJudge])
+          roomsToJudgeOutputs.set(room, [newJudge]);
         }
       }
     }
@@ -29,53 +49,68 @@ export function sortJudgesAndPeople(allTimes: string[], allJudgingRooms: string[
   // people assignment
   let curTimeSlotIdx = 0;
   while (unassignedTeams.length > 0) {
-    const timeSlot = allTimes.at(curTimeSlotIdx);
-    for (let roomInTimeSlot = 0; roomInTimeSlot < allJudgingRooms.length; roomInTimeSlot++) {
+    const timeSlot = allTimes[curTimeSlotIdx];
+    for (
+      const room of allJudgingRooms
+    ) {
       if (unassignedTeams.length == 0) {
         break;
       }
-      const teamToAssign = unassignedTeams.pop();
-      const roomToAssignTo = allJudgingRooms.at(roomInTimeSlot);
-      const hackerOutput: HackerOutput = assignTeamToTime(timeSlot, teamToAssign, roomToAssignTo, hashMap);
-      hackerTable.push(hackerOutput)
+      const teamToAssignIndx = Math.floor(
+        Math.random() * unassignedJudges.length + 1
+      );
+      const teamToAssign = unassignedTeams[teamToAssignIndx] // select a random team from the unassigned teams
+      unassignedTeams.splice(teamToAssignIndx, 1);
+      const hackerOutput: HackerOutput = assignTeamToTime(
+        timeSlot,
+        teamToAssign,
+        room,
+        roomsToJudgeOutputs
+      );
+      hackerTable.push(hackerOutput);
     }
     curTimeSlotIdx++;
   }
-  const finalOutput: FinalOutputTables = {
-    judgeOutput: judgesTable, 
-    hackerOutput: hackerTable
-  }
-  return finalOutput
+
+  return finalOutput;
 }
 
 // returns one hackertable entry
-// mutates every single JudgeOutput the map maps to 
-function assignTeamToTime(timeSlot: string, hackerTeam: HackerTeam, room: string, dictionary: Map<string, JudgeOutput[]>): HackerOutput {
-  const judgeGroup: JudgeOutput[] = dictionary.get(room)
-  for (let judge of judgeGroup) {
-    judge.project=hackerTeam.name
-    judge.devPostLink=hackerTeam.devpostLink
+// mutates every single JudgeOutput the map maps to
+// return HackerOutput object, mutates the judge passed in
+function assignTeamToTime(
+  timeSlot: string,
+  hackerTeam: HackerTeam,
+  room: string,
+  roomToJudgeOutput: Map<string, JudgeOutput[]>
+): HackerOutput {
+  const judgeGroup: JudgeOutput[] = roomToJudgeOutput.get(room);
+  for (const judge of judgeGroup) {
+    judge.project = hackerTeam.name;
+    judge.devPostLink = hackerTeam.devpostLink;
   }
-  const judgeNames: string[] = judgeGroup.map(judgeOutput=>judgeOutput.judge)
+  const judgeNames: string[] = judgeGroup.map(
+    (judgeOutput) => judgeOutput.judge
+  );
   const hackerOutput: HackerOutput = {
-    project: hackerTeam.name, 
-    time: timeSlot, 
-    judges: judgeNames, 
+    project: hackerTeam.name,
+    time: timeSlot,
+    judges: judgeNames,
     room: room
-  }
-  return hackerOutput
+  };
+  return hackerOutput;
 }
 
-// judges will always bein the same room for all timeslots
+// judges will always be in the same room for all timeslots
+// no mutations, returns the new judge
 function assignJudgeToRoom(judge: string, room: string): JudgeOutput {
   const newJudge: JudgeOutput = {
     room: room,
     judge: judge,
-    project: "",
-    devPostLink: "",
-    inPersonDemo: false, 
-    time: ""
-  }
-  return newJudge
+    project: '',
+    devPostLink: '',
+    inPersonDemo: false,
+    time: ''
+  };
+  return newJudge;
 }
-
