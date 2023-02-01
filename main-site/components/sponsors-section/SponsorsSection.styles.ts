4980b9dc3371b66f8e6e9af11607f66e25587846
jzhzhang66 <jzhzhang66@gmail.com>
2023-02-01 12:05:26 -0500

    finished sponsors section desktop

--- main-site/components/sponsors-section/SponsorsSection.styles.ts
+++ main-site/components/sponsors-section/SponsorsSection.styles.ts
@@ -1,9 +1,55 @@
 import styled from 'styled-components';
-import { H2 } from '../../../shared-ui/style/typography';
+import { max } from '../../../shared-ui/lib/responsive';
+import { colors } from '../../../shared-ui/style/colors';
+import { H2, H4 } from '../../../shared-ui/style/typography';
 
-const StyledSponsorsHeader = styled(H2)`
-  display: flex;
-  justify-content: center;
+const StyledSponsorsSectionContainer = styled.div`
+  text-align: center;
+  margin: 10em 0 5em 0;
+  position: relative;
 `;
 
-export { StyledSponsorsHeader };
+const StyledSponsorsHeader = styled(H2)`
+  margin-bottom: 1em;
+`;
+
+const StyledContactText = styled(H4)`
+  color: ${colors.TEXT_BROWN};
+  padding: 3em 12em;
+  @media ${max.desktop} {
+    padding: 3em;
+  }
+`;
+
+const StyledDiamondLogo = styled.img`
+  margin: 4em 0;
+`;
+
+const StyledGoldLogos = styled.img`
+  margin-bottom: 3em;
+`;
+
+const StyledSilverLogos = styled.img`
+  margin-top: 3em;
+`;
+
+const StyledTreasureChest = styled.img`
+  position: absolute;
+  right: -2em;
+  top: -8em;
+  @media ${max.desktop} {
+    width: 20em;
+    right: 0;
+    top: -5em;
+  }
+`;
+
+export {
+  StyledSponsorsSectionContainer,
+  StyledDiamondLogo,
+  StyledGoldLogos,
+  StyledSilverLogos,
+  StyledSponsorsHeader,
+  StyledContactText,
+  StyledTreasureChest
+};
--- main-site/components/sponsors-section/SponsorsSection.tsx
+++ main-site/components/sponsors-section/SponsorsSection.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import SecondaryButton from '../../../shared-ui/components/secondary-button/SecondaryButton';
-import { H2 } from '../../../shared-ui/style/typography';
-import { StyledSponsorsHeader } from './SponsorsSection.styles';
+import {
+  StyledSponsorsSectionContainer,
+  StyledDiamondLogo,
+  StyledGoldLogos,
+  StyledSilverLogos,
+  StyledSponsorsHeader,
+  StyledContactText,
+  StyledTreasureChest
+} from './SponsorsSection.styles';
+import DiamondLogoDesktop from '../../images/diamond-level-logo-desktop.png';
+import GoldLogosDesktop from '../../images/gold-level-logos-desktop.png';
+import SilverLogosDesktop from '../../images/silver-level-logos-desktop.png';
+import TreasureChest from '../../images/treasure-chest.svg';
+import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
 
 const SponsorsSection: React.FC = () => {
   return (
-    <div>
+    <StyledSponsorsSectionContainer>
+      <StyledTreasureChest src={TreasureChest} alt="treasure chest" />
       <StyledSponsorsHeader>2023 Sponsors</StyledSponsorsHeader>
-      <SecondaryButton btnText="Diamond Level" />
-      <SecondaryButton btnText="Gold Level" />
-      <SecondaryButton btnText="Silver Level" />
-    </div>
+      <div>
+        <SecondaryButton btnText="Diamond Level" isClickable={false} />
+        <br />
+        <StyledDiamondLogo src={DiamondLogoDesktop} alt="diamond level logo" />
+      </div>
+      <div>
+        <SecondaryButton btnText="Gold Level" isClickable={false} />
+        <br />
+        <StyledGoldLogos src={GoldLogosDesktop} alt="gold level logos" />
+      </div>
+      <div>
+        <SecondaryButton btnText="Silver Level" isClickable={false} />
+        <br />
+        <StyledSilverLogos src={SilverLogosDesktop} alt="silver level logos" />
+      </div>
+      <StyledContactText>
+        Interested in joining the HackBeanpot universe? <br /> <br /> Reach out
+        to us at team@hackbeanpot.com or check out our sponsorship packet!
+      </StyledContactText>
+      <PrimaryButton btnText="View packet" />
+    </StyledSponsorsSectionContainer>
   );
 };
 
