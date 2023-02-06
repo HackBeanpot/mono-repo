6c3b0d57cb57268cc186544849a11a0d9d5a1fc7
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2023-02-05 21:39:23 -0500

    use state done

--- live-site/components/judging-section/judging-schedule-welcome/JudgingScheduleWelcome.tsx
+++ live-site/components/judging-section/judging-schedule-welcome/JudgingScheduleWelcome.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyledBackButtonContainer,
   StyledBackButton,
@@ -8,12 +8,14 @@ import {
   StyledWelcomePerson
 } from '../judging-schedule-welcome/JudgingScheduleWelcome.styles';
 import { JudgingScheduleWelcomeProps } from '../../../lib/types';
+import JudgingScheduleTable from '../judging-schedule-table/JudgingScheduleTable';
 
 const JudgingScheduleWelcome: React.FC<JudgingScheduleWelcomeProps> = ({
   schedulePersonType
 }) => {
-  const personOptions = ['Select your name'];
-
+  const personOptions = ['Select your name', 'Lisa', 'Surbhs']; // inside judges.json => only do the keys
+  const [personSelected, setPersonSelected] =
+    useState<string>('Select your name');
   return (
     <div>
       <StyledBackButtonContainer>
@@ -24,14 +26,22 @@ const JudgingScheduleWelcome: React.FC<JudgingScheduleWelcomeProps> = ({
           Welcome, {schedulePersonType}!
         </StyledWelcomePerson>
         <StyledJudgesDropdownContainer>
-          <StyledJudgesDropdownWrapper id="position-filter">
+          <StyledJudgesDropdownWrapper
+            id="position-filter"
+            onChange={(e): void => {
+              setPersonSelected(e.target.value);
+            }}
+          >
+            {/* this -- above-- is a select */}
             {personOptions.map((currPerson: string) => (
+              // on change, then we want to set the person differently
               <option value={currPerson} key={currPerson}>
                 {currPerson}
               </option>
             ))}
           </StyledJudgesDropdownWrapper>
         </StyledJudgesDropdownContainer>
+        {personSelected != 'Select your name' && <JudgingScheduleTable headers={['hi', 'hi', 'hi', 'hi']} rows={[['lol', 'lol', 'lol', 'lol']]}/>}
       </StyledJudgingScheduleSection>
     </div>
   );
