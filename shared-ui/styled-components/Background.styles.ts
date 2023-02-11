d32badadaceef68ac41d5f284b567b7ab011c7c9
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2023-02-11 11:20:39 -0500

    ljasdf

--- shared-ui/styled-components/Background.styles.ts
+++ shared-ui/styled-components/Background.styles.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components';
 
 const StyledBackgrounds = styled.img`
   width: 100%;
+  height: 150%;
   position: absolute;
+  overflow-x: hidden;
   z-index: -1;
+  object-fit: cover;
 `;
 
 const StyledPageContainer = styled.div`
