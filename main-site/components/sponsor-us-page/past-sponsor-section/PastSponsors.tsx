696aec8dd97ea2a6c59e9f212bc68d05d48d6f45
aaryan1203 <aaryanjain1203@gmail.com>
2023-08-29 00:27:06 -0400

    Text under past sponsors image only shows up on desktop

--- main-site/components/sponsor-us-page/past-sponsor-section/PastSponsors.tsx
+++ main-site/components/sponsor-us-page/past-sponsor-section/PastSponsors.tsx
@@ -1,47 +1,51 @@
 import React from 'react';
-import { 
-    StyledPastSponsorsContainer, 
-    StyledPastSponsorsImagesContainer,
-    StyledTextSpacing,
-    StyledCenterImage,
-    StyledBlurbText,
-    StyledButtonContainer
-} from './PastSponsor.styles'
+import {
+  StyledPastSponsorsContainer,
+  StyledPastSponsorsImagesContainer,
+  StyledTextSpacing,
+  StyledCenterImage,
+  StyledBlurbText,
+  StyledButtonContainer
+} from './PastSponsor.styles';
 import { H2 } from '../../../../shared-ui/style/typography';
 import PrimaryButton from '../../../../shared-ui/components/primary-button/PrimaryButton';
 import { breakpoints } from '../../../../shared-ui/lib/responsive';
 import DesktopSizeImage from '../../../../shared-ui/images/sponsor-us/desktop-sponsors.png';
 import MobileSizeImage from '../../../../shared-ui/images/sponsor-us/mobile-sponsors.png';
 
-
 const PastSponsors: React.FC = () => {
-    return (
-        <StyledPastSponsorsContainer>
-            <H2>Past Sponsors</H2>
-            <StyledPastSponsorsImagesContainer>
-            <StyledCenterImage src={window.innerWidth <= breakpoints.mobile ? MobileSizeImage : DesktopSizeImage}/>
-            <StyledTextSpacing>
-                Due to HackBeanpot's status as a 501c3 nonprofit organization, all sponsorship package
-                purchases are considered tax deductible.
-            </StyledTextSpacing>
-            </StyledPastSponsorsImagesContainer>
+  return (
+    <StyledPastSponsorsContainer>
+      <H2>Past Sponsors</H2>
+      <StyledPastSponsorsImagesContainer>
+        <StyledCenterImage
+          src={
+            window.innerWidth <= breakpoints.mobile
+              ? MobileSizeImage
+              : DesktopSizeImage
+          }
+        />
+        {window.innerWidth > breakpoints.mobile && (
+          <StyledTextSpacing>
+            Due to HackBeanpot's status as a 501c3 nonprofit organization, all
+            sponsorship package purchases are considered tax deductible.
+          </StyledTextSpacing>
+        )}
+      </StyledPastSponsorsImagesContainer>
 
-            <StyledBlurbText>
-                Interested in joining the HackBeanpot universe?
-                <br/><br/>
-                We would love to hear from you! Reach out to us at core@hackbeanpot.com 
-                to discuss further and thank you for considering us.
-            </StyledBlurbText>
+      <StyledBlurbText>
+        Interested in joining the HackBeanpot universe?
+        <br />
+        <br />
+        We would love to hear from you! Reach out to us at core@hackbeanpot.com
+        to discuss further and thank you for considering us.
+      </StyledBlurbText>
 
-            <StyledButtonContainer>
-            <PrimaryButton
-                btnText="Contact Us"
-                btnLink="#"
-            />
-            </StyledButtonContainer>
-        </StyledPastSponsorsContainer>
-    );
+      <StyledButtonContainer>
+        <PrimaryButton btnText="Contact Us" btnLink="#" />
+      </StyledButtonContainer>
+    </StyledPastSponsorsContainer>
+  );
 };
-
 
 export default PastSponsors;
