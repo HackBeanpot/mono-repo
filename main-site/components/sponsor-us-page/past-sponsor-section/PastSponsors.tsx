9e4050701127fd6a6106f10dfcaf4e8133304a74
Mandy-cyber <acm.rodriques@gmail.com>
2023-09-06 14:05:36 -0400

    now using useMatchMedia for responsive image

--- main-site/components/sponsor-us-page/past-sponsor-section/PastSponsors.tsx
+++ main-site/components/sponsor-us-page/past-sponsor-section/PastSponsors.tsx
@@ -9,23 +9,19 @@ import {
 } from './PastSponsor.styles';
 import { H2 } from '../../../../shared-ui/style/typography';
 import PrimaryButton from '../../../../shared-ui/components/primary-button/PrimaryButton';
-import { breakpoints } from '../../../../shared-ui/lib/responsive';
+import { min } from '../../../../shared-ui/lib/responsive';
+import useMatchMedia from 'react-use-match-media';
 import DesktopSizeImage from '../../../../shared-ui/images/sponsor-us/desktop-sponsors.png';
 import MobileSizeImage from '../../../../shared-ui/images/sponsor-us/mobile-sponsors.png';
 
 const PastSponsors: React.FC = () => {
+  const isDesktop = useMatchMedia(min.mobile);
   return (
     <StyledPastSponsorsContainer>
       <H2>Past Sponsors</H2>
       <StyledPastSponsorsImagesContainer>
-        <StyledCenterImage
-          src={
-            window.innerWidth <= breakpoints.mobile
-              ? MobileSizeImage
-              : DesktopSizeImage
-          }
-        />
-        {window.innerWidth > breakpoints.mobile && (
+        <StyledCenterImage src={ isDesktop ? DesktopSizeImage : MobileSizeImage}/>
+        {isDesktop && (
           <StyledImageDescription>
             Due to HackBeanpot's status as a 501c3 nonprofit organization, all
             sponsorship package purchases are considered tax deductible.
