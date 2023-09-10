335b4a438775cf7b1e3d7747aec47ee8ada8ece5
kzeng24 <kathyzeng2002@gmail.com>
2023-09-10 12:33:56 -0400

    mobile view of sponsor us landing and why sponsor section

--- main-site/components/sponsor-us-page/sponsor-landing-page/SponsorLandingPage.styles.ts
+++ main-site/components/sponsor-us-page/sponsor-landing-page/SponsorLandingPage.styles.ts
@@ -1,42 +1,67 @@
 import styled from 'styled-components';
-import { min } from '../../../../shared-ui/lib/responsive';
+import { max, min } from '../../../../shared-ui/lib/responsive';
 import { colors } from '../../../../shared-ui/style/colors';
 import { H2, P } from '../../../../shared-ui/style/typography';
 
 const StyledContainer = styled.div`
   position: relative;
-  padding-top: 21em;
-  `;
+
+  @media ${min.desktop} {
+    padding-top: 21em;
+  }
+
+  @media ${max.tablet} {
+    padding-top: 10em;
+  }
+`;
 
 const StyledSponsorUsHeader = styled(H2)`
   color: ${colors.TEXT_BROWN};
   text-align: left;
   letter-spacing: 1px;
-  font-size: 4.5em;
+
+  @media ${min.desktop} {
+    font-size: 4.5em;
+  }
+
+  @media ${max.tablet} {
+    font-size: 2em;
+  }
 `;
 
 const StyledSponsorUsSectionContainer = styled.div`
-  padding-left: 5em;
-  padding-right: 10em;
-  padding-bottom: 5em;
-  border-radius: 3em;
+  position: relative;
 
-   @media ${min.desktop} {
+  @media ${min.desktop} {
+    padding-left: 5em;
+    padding-right: 10em;
+    padding-bottom: 5em;
+    border-radius: 3em;
     width: 50em;
-
   }
-  @media ${min.desktopLg} {
-    width: 60em;
+
+  @media ${max.tablet} {
+    padding: 2em;
   }
 `;
 
 const StyledParagraph = styled(P)`
   text-align: left;
-  padding: 1.5em 0;
+  padding: 1em 0;
+  font-size: 1.1em;
 `;
 
 const StyledButtonContainer = styled.div`
-  text-align: left;
+  @media ${min.desktop} {
+    text-align: left;
+  }
+
+  @media ${max.tablet} {
+    justify-content: center;
+    align-items: center;
+    padding: 0.5em;
+    font-size: 1em;
+  }
 `;
 
 export {StyledSponsorUsHeader, StyledSponsorUsSectionContainer, StyledParagraph, StyledButtonContainer, StyledContainer};
