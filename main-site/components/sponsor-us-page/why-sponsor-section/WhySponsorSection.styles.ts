15dace63f3684f7a0856d46a3709046a3adb41dd
Katherine Zeng <52469441+kzeng24@users.noreply.github.com>
2023-09-17 22:22:08 -0400

    [Enhancment] Mobile Landing Page + Help Your Company Sections (#222)
    * mobile view of sponsor us landing and why sponsor section
    
    * added tablet view + fixed dependency errors

--- main-site/components/sponsor-us-page/why-sponsor-section/WhySponsorSection.styles.ts
+++ main-site/components/sponsor-us-page/why-sponsor-section/WhySponsorSection.styles.ts
@@ -1,49 +1,71 @@
 import styled from 'styled-components';
 import { colors } from '../../../../shared-ui/style/colors';
 import { H4, P } from '../../../../shared-ui/style/typography';
-import { min } from '../../../../shared-ui/lib/responsive';
+import { max, min } from '../../../../shared-ui/lib/responsive';
 
 const StyledWhySponsorContainer = styled.div`
   position: relative;
   display: flex;
   justify-content: center;
-  position: relative;
-  padding-bottom: 10em;
 
-  @media ${min.mobile} {
-    margin-top: 14em;
-  }
-  @media ${min.tabletSm} {
-    margin-top: 24em;
+  @media ${max.tablet} {
+    margin: 1.5em;
+    padding-bottom: 5em;
   }
   @media ${min.tablet} {
-    margin-top: 4em;
-  }
-  @media ${min.tabletLg} {
-    margin-top: 10em;
+    margin: 3em;
+    padding-bottom: 7em;
   }
   @media ${min.desktop} {
     padding-top: 5em;
+    padding-bottom: 10em;
+  }
+`;
+
+const HideInMobileView = styled.div`
+   @media ${max.tablet} {
+    display: none;
+  }
+  @media ${min.tablet} {
+    display: block;
+  }
+  @media ${min.desktop} {
+    display: block;
+  }
+`;
+
+const ShowInMobileView = styled.div`
+  @media ${max.tablet} {
+    display: block;
+  }
+  @media ${min.tablet} {
+    display: none;
+  }
+  @media ${min.desktop} {
+    display: none;
   }
 `;
 
 const StyledTextContainer = styled.div`
-  padding: 4em;
-  border-radius: 3em;
   margin-top: 0;
   background-color: ${colors.TEXT_BOX};
 
-  @media ${min.tabletSm} {
-    width: 20em;
-  }
-  @media ${min.tablet} {
+  @media ${max.tablet} {
     width: 38em;
+    padding: 1.5em;
+    border-radius: 1em;
   }
-  @media ${min.tabletLg} {
-    width: 52em;
+
+   @media ${min.tablet} {
+    width: 43em;
+    padding: 2.5em;
+    border-radius: 2em;
   }
+
   @media ${min.desktop} {
     width: 52em;
+    padding: 4em;
+    border-radius: 3em;
   }
 `;
 
@@ -51,10 +73,18 @@ const StyledHeaderLabel = styled(H4)`
   color: white;
   margin-bottom: 0.5em;
   letter-spacing: 1px;
+
+  @media ${max.tablet} {
+    font-size: 1.4em;
+  }
 `;
 
 const StyledParagraph = styled(P)`
-  padding-top: 1.5em;
+  padding-top: 1.7em;
+
+  @media ${max.tablet} {
+    font-size: 0.9em;
+  }
 `;
 
 const StyledTextSpacing = styled(P)`
@@ -64,23 +94,26 @@ const StyledTextSpacing = styled(P)`
 const StyledFennecFox = styled.img`
   position: absolute;
 
-  @media ${min.tabletSm} {
+  @media ${max.tablet} {
     display: none;
   }
+
   @media ${min.tablet} {
     right: 2em;
-    top: 25em;
+    top: 30em;
     height: 15em;
     width: 15em;
     display: block;
   }
- @media ${min.desktop} {
+
+  @media ${min.desktop} {
     right: 7em;
     top: 30em;
     height: 20em;
     width: 20em;
     display: block;
   }
+
   @media ${min.desktopLg} {
     right: 20em;
     top: 30em;
@@ -107,5 +140,7 @@ export {
   StyledTextSpacing,
   StyledFennecFox,
   StyledItemContainer,
-  StyledCenterImage
+  StyledCenterImage,
+  HideInMobileView,
+  ShowInMobileView
 };
