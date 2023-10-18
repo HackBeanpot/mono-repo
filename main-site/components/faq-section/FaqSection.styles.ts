73e1a4fc513a29305e78a797a6549625510b1632
jmsargent10 <120346417+jmsargent10@users.noreply.github.com>
2023-10-18 01:28:17 -0400

    Revert "[Feature] FAQ Refresh (#244)" (#252)
    This reverts commit 889475c094c598b874cf6bfbcd2b14340de8cc7f.

--- main-site/components/faq-section/FaqSection.styles.ts
+++ main-site/components/faq-section/FaqSection.styles.ts
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
-import { StyledPrimaryButton } from '../../../shared-ui/components/primary-button/PrimaryButton.styles';
 import { max } from '../../../shared-ui/lib/responsive';
 import { colors } from '../../../shared-ui/style/colors';
-import { fonts, H2, H4 } from '../../../shared-ui/style/typography';
-import { ButtonProps } from '../../../shared-ui/lib/types';
+import { H4 } from '../../../shared-ui/style/typography';
 
 const StyledFaqSectionContainer = styled.div`
   text-align: center;
@@ -11,91 +9,23 @@ const StyledFaqSectionContainer = styled.div`
   margin-top: 8em;
 `;
 
-const StyledOctopus = styled.img`
+const StyledCactus = styled.img`
   position: absolute;
-  top: -7em;
-  right: -1.5em;
-  width: 18em;
-  height: 21em;
+  margin-left: 5em;
+  top: 0.1em;
+  left: 1em;
 
-@media ${max.tabletLg} {
-    width: 14em;
-    height: 17em;
-}
-
-@media ${max.tablet} {
-    width: 10em;
-    height: 13em;
-    right: 0em;
-}
-
- @media ${max.tabletXs} {
-  width: 8em;
-  height: 11em;
-  right: 0em;
-}
-
-`;
-
-const StyledLightHouse = styled.img`
-  position: absolute;
-  top: 32em;
-  left: -5em;
-  width: 27em;
-  height: 31em;
-
-@media ${max.tabletLg} {
-    width: 23em;
-    height: 20em;
-    top: 45em;
-}
-
-@media ${max.tablet} {
+  @media ${max.desktop} {
     width: 13em;
-    height: 16em;
-    left: -4em;
-    top: 35em;
-}
-
- @media ${max.tabletXs} {
-  width: 10em;
-  height: 13em;
-  left: -2.5em;
-  top: 40em;
-}
-`;
-
-const StyledH4 = styled(H4)`
-  color: ${colors.WHITE} !important;
-  font-family: ${fonts.nunitoRegular};
-  margin-top: 1em;
-  padding: 2em;
-  font-weight: 400;
-`;
-
-const StyledFaqSectionTitle = styled(H2)`
-  color: ${colors.WHITE};
-  font-family: ${fonts.nunitoRegular};
-
-  @media ${max.tablet} {
-    font-size: 2em;
-`;
-
-const StyledContactUsButton = styled(StyledPrimaryButton)<ButtonProps>`
-  color: #FABB32;
-  background-color: #FABB32;
-  font-family: ${fonts.nunitoRegular};
-  &:hover {
-    color: #D29000
-    background-color: #D29000
+    top: -1em;
+    display: none;
   }
 `;
 
-export { 
-  StyledOctopus,
-  StyledLightHouse, 
-  StyledFaqSectionContainer, 
-  StyledH4,
-  StyledFaqSectionTitle,
-  StyledContactUsButton
-};
+const StyledH4 = styled(H4)`
+  color: ${colors.TEXT_BROWN} !important;
+  margin-top: 3em;
+  padding: 2em;
+`;
+
+export { StyledCactus, StyledFaqSectionContainer, StyledH4 };
