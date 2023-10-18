84d5c0086ee5c42a3b505b4c033ed08d310bcb24
Johny Sargent <johnysargent@MacBook-Pro-3.local>
2023-10-18 17:21:54 -0400

    adding changes from old branch

--- main-site/components/faq-section/faq/Faq.tsx
+++ main-site/components/faq-section/faq/Faq.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import { FaqSectionData } from '../../../lib/data';
 import FaqItem from '../faq-item/FaqItem';
-import { StyledFaqContainer } from './Faq.styles';
+import { StyledFaqContainer, StyledFaqSubtitle } from './Faq.styles';
 
 const Faq: React.FC = () => {
   const [selectedQuestion, setSelectedQuestion] = useState<number>(0);
+  
   return (
     <StyledFaqContainer>
       {FaqSectionData.map((data) => (
-        <FaqItem
-          key={`faq-${data.id}`}
-          id={data.id}
-          question={data.question}
-          answer={data.answer}
-          selectedQuestion={selectedQuestion}
-          setSelectedQuestion={setSelectedQuestion}
-        />
+        <>
+          {data.id === 1 && <StyledFaqSubtitle>Time & Location</StyledFaqSubtitle>}
+          {data.id === 4 && <StyledFaqSubtitle>Application Logistics</StyledFaqSubtitle>}
+          {data.id === 5 && <StyledFaqSubtitle>Event Logistics</StyledFaqSubtitle>}
+          <FaqItem
+            key={`faq-${data.id}`}
+            id={data.id}
+            question={data.question}
+            answer={data.answer}
+            selectedQuestion={selectedQuestion}
+            setSelectedQuestion={setSelectedQuestion}
+          />
+        </>
       ))}
     </StyledFaqContainer>
   );
