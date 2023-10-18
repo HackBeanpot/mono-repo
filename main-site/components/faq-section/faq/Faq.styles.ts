db3a84dc15c69af4da3e537b6b6e3997684223a7
jmsargent10 <120346417+jmsargent10@users.noreply.github.com>
2023-10-18 01:12:20 -0400

    Revert "[Feature] FAQ Refresh (#244)"
    This reverts commit 889475c094c598b874cf6bfbcd2b14340de8cc7f.

--- main-site/components/faq-section/faq/Faq.styles.ts
+++ main-site/components/faq-section/faq/Faq.styles.ts
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
 import { max } from '../../../../shared-ui/lib/responsive';
 import { colors } from '../../../../shared-ui/style/colors';
-import { fonts, H4 } from '../../../../shared-ui/style/typography';
 
-const StyledFaqContainer = styled.div`
+export const StyledFaqContainer = styled.div`
   position: relative;
   width: 60%;
   margin: 0 auto;
@@ -13,18 +12,7 @@ const StyledFaqContainer = styled.div`
   text-align: left;
 
 @media ${max.tablet} {
-    width: 60%;
+    width: 80%;
     padding: 0em; 
 }
 `;
-
-const StyledFaqSubtitle = styled(H4)`
-  font-family: ${fonts.nunitoRegular};
-  color: ${colors.WHITE};
-  margin-top: 1em;
-`;
-
-export {
-  StyledFaqContainer,
-  StyledFaqSubtitle
-};
--- main-site/lib/data.ts
+++ main-site/lib/data.ts
@@ -43,9 +43,9 @@ export const aboutSectionData: AboutSectionData[] = [
 export const FaqSectionData: FaqData[] = [
   {
     id: 1,
-    question: 'When and where is HackBeanpot 2023?',
+    question: 'How do I apply to HackBeanpot?',
     answer:
-      'HackBeanpot 2023 will take place on February 10th-12th at the Wood Mackenzie (formerly known as Power Advocate) office building at 179 Lincoln St, Boston, MA, 02111!  We are so excited to be back in person for the first time since 2020, and we ask that all hackers review and abide by the Code of Conduct during the event.'
+      'Apply through our application portal at apply.hackbeanpot.com! Keep an eye in your inbox for acceptance details in mid-January. Applications close January 20th, 2023.'
   },
 
   {
@@ -57,84 +57,66 @@ export const FaqSectionData: FaqData[] = [
 
   {
     id: 3,
-    question: 'How long is the event?',
+    question: 'When and where is HackBeanpot 2023?',
     answer:
-    'The event will last an entire weekend in February, starting at around 7pm EST the Friday leading into the weekend and continuing until 3:30pm EST on Sunday. Please note that these times are tentative and subject to change as we get closer to the event date - please check our Instagram (@hackbeanpot) for the most updated information! Throughout the weekend, there will be different workshops, activities, and opportunities to network with sponsors. A more detailed schedule will be released about a month out from the event. Sign up for our newsletter to stay updated!'
+      'HackBeanpot 2023 will take place on February 10th-12th at the Wood Mackenzie (formerly known as Power Advocate) office building at 179 Lincoln St, Boston, MA, 02111!  We are so excited to be back in person for the first time since 2020, and we ask that all hackers review and abide by the Code of Conduct during the event.'
   },
-  
   {
     id: 4,
-    question: 'How do I apply to attend HackBeanpot?',
+    question: 'What is HackBeanpot doing about COVID safety?',
     answer:
-      'Apply through our application portal at apply.hackbeanpot.com! Keep an eye in your inbox for acceptance details in mid-January. Applications close January 20th, 2023.'
+      'HackBeanpot is committed to ensuring that hackers feel safe and activities comply with local COVID-19 policies. For this reason, we ask that hackers upload proof of vaccination when asked after registration and encourage everyone to wear masks during the event as it is indoors.'
   },
 
   {
     id: 5,
+    question: 'How long is the event? / What will the schedule be like?',
+    answer:
+      'The event will last an entire weekend in February, starting at around 7pm EST the Friday leading into the weekend and continuing until 3:30pm EST on Sunday. Please note that these times are tentative and subject to change as we get closer to the event date - please check our Instagram (@hackbeanpot) for the most updated information! Throughout the weekend, there will be different workshops, activities, and opportunities to network with sponsors. A more detailed schedule will be released about a month out from the event. Sign up for our newsletter to stay updated!'
+  },
+
+  {
+    id: 6,
     question: 'What kind of projects can I work on?',
     answer:
       'The desert is yours to explore! We encourage you to work on anything you want including websites, mobile apps, data visualizations, games, etc. Our prizes are meant to be very broad so that the direction of your project can be determined by your passions and what you want to learn! Feel free to check out some of our past projects for inspiration. Please note that you are free to work on past projects if you want to, but these will not qualify for prizes as this is unfair to other hackers!'
   },
 
   {
-    id: 6,
-    question: 'How do I find a team?',
+    id: 7,
+    question: 'How do I find a team? / Are there limits on team size?',
     answer:
       'There will be a team formation activity at the beginning of the hackathon where you can meet your dream team! Another way of finding team members is through our event Slack, which will be sent out one week before the event. You are also free to attend with a premade team. While there isn’t a limit on team sizes, our prizes are budgeted for teams of up to 5 members.'
   },
 
   {
-    id: 7,
-    question: 'What prizes are there this year?',
+    id: 8,
+    question: 'Who can attend HackBeanpot?',
     answer:
-      '** fill in here **'
+      'HackBeanpot is open to all high school, undergraduate, and graduate school students. You must be 13 years or older to apply. All attendees must abide by our Code of Conduct.'
+  },
+
+  {
+    id: 9,
+    question:
+      "I don't have a lot of programming experience. Are hackathons for me?",
+    answer:
+      'Yes, people of all experience levels attend HackBeanpot, including those who have never coded before! We plan to have many resources to help beginner hackers with their projects such as a tech starter pack with tutorials on how to get started, common tools used at hackathons, and how to resolve common debugging issues. You can also attend several beginner workshops and talks to help you learn about new skills and technologies throughout the weekend! If you get stuck, we have plenty of mentors to provide assistance. We care about fostering a fun hacking environment rather than a competitive one, and our goal is to promote learning in an inclusive community.'
+  },
+
+  {
+    id: 10,
+    question: 'How much does it cost? Will there be swag?',
+    answer:
+      'HackBeanpot is completely free for all our attendees, including meals and snacks! We will also be giving out free swag so you can ~ thrive in our desert oasis ~!'
+  },
+
+  {
+    id: 11,
+    question: 'What will I need to bring?',
+    answer:
+      'To have a great time at HackBeanpot 2023, you should arrive with your laptop and a curious mindset fit for an explorer! Stay tuned for a Desert Exploration Guide in your inbox sent out closer to the event with more details.'
   }
-
-  // {
-  //   id: 2,
-  //   question: 'Will HackBeanpot 2023 be in-person or virtual?',
-  //   answer:
-  //     'HackBeanpot 2023 will be in-person! We will be requiring proof of vaccination for attendees, and we’re super excited to see everyone in person again!'
-  // },
-
-  // {
-  //   id: 4,
-  //   question: 'What is HackBeanpot doing about COVID safety?',
-  //   answer:
-  //     'HackBeanpot is committed to ensuring that hackers feel safe and activities comply with local COVID-19 policies. For this reason, we ask that hackers upload proof of vaccination when asked after registration and encourage everyone to wear masks during the event as it is indoors.'
-  // },
-
-
-
-
-  // {
-  //   id: 8,
-  //   question: 'Who can attend HackBeanpot?',
-  //   answer:
-  //     'HackBeanpot is open to all high school, undergraduate, and graduate school students. You must be 13 years or older to apply. All attendees must abide by our Code of Conduct.'
-  // },
-
-  // {
-  //   id: 9,
-  //   question:
-  //     "I don't have a lot of programming experience. Are hackathons for me?",
-  //   answer:
-  //     'Yes, people of all experience levels attend HackBeanpot, including those who have never coded before! We plan to have many resources to help beginner hackers with their projects such as a tech starter pack with tutorials on how to get started, common tools used at hackathons, and how to resolve common debugging issues. You can also attend several beginner workshops and talks to help you learn about new skills and technologies throughout the weekend! If you get stuck, we have plenty of mentors to provide assistance. We care about fostering a fun hacking environment rather than a competitive one, and our goal is to promote learning in an inclusive community.'
-  // },
-
-  // {
-  //   id: 10,
-  //   question: 'How much does it cost? Will there be swag?',
-  //   answer:
-  //     'HackBeanpot is completely free for all our attendees, including meals and snacks! We will also be giving out free swag so you can ~ thrive in our desert oasis ~!'
-  // }
-
-  // {
-  //   id: 11,
-  //   question: 'What will I need to bring?',
-  //   answer:
-  //     'To have a great time at HackBeanpot 2023, you should arrive with your laptop and a curious mindset fit for an explorer! Stay tuned for a Desert Exploration Guide in your inbox sent out closer to the event with more details.'
-  // }
 ];
 
 export const testimonialSectionData: TestimonialData[] = [
