e0fc4fb40889fcaf3ff3bbd4da6f1e9fb7e7be7b
unknown <nelsondong158@gmail.com>
2023-11-03 11:47:42 -0400

    variants

--- shared-ui/animations/variants.ts
+++ shared-ui/animations/variants.ts
@@ -1,40 +1,25 @@
-const customAnimationVariants = {
-  initial: { opacity: 0 },
-  visible: {
+const container = {
+  hidden: { opacity: 0, y: '50vh' },
+  staggerFadeInAndUp: {
     opacity: 1,
-    y: 0,
+    y: '0',
     transition: {
-      when: 'afterChildren'
+      staggerChildren: 0.2
     }
   },
-  hidden: {
-    opacity: 0,
-    y: '25vh',
-    transition: {
-      when: 'beforeChildren',
-      staggerChildren: 0.08,
-      delayChildren: 0
-    }
-  }
-};
-
-const containerAnimation = {
-  animate: {
-    transition: {
-      staggerChildren: 0.5
-    }
-  }
-};
-
-const itemAnimation = {
-  initial: { opacity: 0, y: 60 },
-  animate: {
+  fadeInAndUp: {
     opacity: 1,
-    y: 0,
-    transition: {
-      duration: 1
-    }
+    y: '0'
   }
 };
 
-export default { customAnimationVariants, containerAnimation, itemAnimation };
+const item = {
+  hidden: { opacity: 0, y: '50vh' },
+  staggerFadeInAndUp: { opacity: 1, y: 0 },
+  fadeInAndUp: {
+    opacity: 1,
+    y: '0'
+  }
+};
+
+export default { container, item };
