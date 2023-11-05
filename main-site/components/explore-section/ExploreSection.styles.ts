2267f3f0fae7d28ed39bed9ded0d30be9ce5eae6
aaryan1203 <aaryanjain1203@gmail.com>
2023-11-05 16:44:58 -0500

    Fixed swimmer swimming into the words

--- main-site/components/explore-section/ExploreSection.styles.ts
+++ main-site/components/explore-section/ExploreSection.styles.ts
@@ -68,6 +68,7 @@ const StyledDiver = styled.img`
   right: 2em;
   top: 17em;
   width: 20em;
+
   @media ${min.tablet} {
     top: 30em;
     right: 6em;
@@ -76,12 +77,26 @@ const StyledDiver = styled.img`
   @media ${min.tabletLg} {
     top: 28em;
   }
-  @media ${max.tabletSm} {
-    top: 108vw;
-    width: 60%;
+  @media (max-width: 767px) {
+    top: 30em;
+    right: 15em;
   }
-  @media ${max.mobile} {
-    margin-top: 10em;
+  @media (max-width: 600px) {
+    top: 32em;
+    right: 5em;
+  }
+  @media (max-width: 540px) {
+    width: 50%;
+    top: 37em;
+  }
+  @media (max-width: 450px) {
+    top: 42em;
+  }
+  @media (max-width: 425px) {
+    top: 45em;
+  }
+  @media (max-width: 390px) {
+    top: 50em;
   }
 `;
 
