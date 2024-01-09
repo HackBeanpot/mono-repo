bae15d7410734532ea19dc4269f4b67690333766
Mandy-cyber <acm.rodriques@gmail.com>
2024-01-09 15:50:59 -0500

    fixed conflicts

--- shared-ui/components/secondary-button/SecondaryButton.tsx
+++ shared-ui/components/secondary-button/SecondaryButton.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { StyledSecondaryButton } from './SecondaryButton.styles';
 import { ButtonProps } from '../../lib/types';
 import { StyledSecondaryHeaderButton } from './SecondaryButtonHeader.styles';
-<<<<<<< HEAD
-=======
 import { StyledLink } from '../../style/typography';
 
->>>>>>> 68dd83ee2593b77f98d857147e44094f55659acc
 
 const SecondaryButton: React.FC<ButtonProps> = ({
   btnText,
@@ -15,11 +12,7 @@ const SecondaryButton: React.FC<ButtonProps> = ({
   inHeader
 }) => {
   return (
-<<<<<<< HEAD
-    <a href={btnLink}>
-=======
     <StyledLink href={btnLink}>
->>>>>>> 68dd83ee2593b77f98d857147e44094f55659acc
       {inHeader ? (
         <StyledSecondaryHeaderButton isClickable={isClickable}>
           {btnText}
@@ -29,11 +22,7 @@ const SecondaryButton: React.FC<ButtonProps> = ({
           {btnText}
         </StyledSecondaryButton>
       )}
-<<<<<<< HEAD
-    </a>
-=======
     </StyledLink>
->>>>>>> 68dd83ee2593b77f98d857147e44094f55659acc
   );
 };
 
