c6899133fb7e7204734787eacf47c71510c2315d
aaryan1203 <aaryanjain1203@gmail.com>
2024-01-16 20:03:10 -0500

    Fixed padding

--- shared-ui/components/meet-the-team/MeetTheTeamSection.styles.ts
+++ shared-ui/components/meet-the-team/MeetTheTeamSection.styles.ts
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
+import { max } from '../../lib/responsive';
 
 const StyledMeetTheTeamSection = styled.div`
   margin-top: 15em;
   padding-top: 12em, 0;
   text-align: center;
   padding-bottom: 15em;
+  @media ${max.tabletLg} {
+    padding-bottom: 0em;
+  } ;
 `;
 
 const StyledImageContainer = styled.div`
