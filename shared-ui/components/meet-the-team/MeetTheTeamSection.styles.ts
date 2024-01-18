86114e4c96268f3c5085a7a60d18fa11fac3b37b
Aaryan Jain <76594216+Aaryan1203@users.noreply.github.com>
2024-01-18 12:56:51 -0500

    Fixed padding (#330)
    * Fixed padding
    
    * Added padding

--- shared-ui/components/meet-the-team/MeetTheTeamSection.styles.ts
+++ shared-ui/components/meet-the-team/MeetTheTeamSection.styles.ts
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
+import { max } from '../../lib/responsive';
 
 const StyledMeetTheTeamSection = styled.div`
   padding-top: 12em, 0;
   text-align: center;
   padding-bottom: 15em;
+  @media ${max.tabletLg} {
+    padding-bottom: 0em;
+  } ;
 `;
 
 const StyledImageContainer = styled.div`
