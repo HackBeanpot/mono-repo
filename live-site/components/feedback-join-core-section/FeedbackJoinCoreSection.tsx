c32d6cd1c30186142a97579567dcb3c9e250b493
Mike <103906123+mike10911@users.noreply.github.com>
2024-01-22 22:00:10 -0500

    Mike/bottom of live site (#334)
    * fix feedback and add bottom of the ocean
    
    * space

--- live-site/components/feedback-join-core-section/FeedbackJoinCoreSection.tsx
+++ live-site/components/feedback-join-core-section/FeedbackJoinCoreSection.tsx
@@ -3,43 +3,45 @@ import {
   StyledColumn,
   StyledColumnText,
   StyledColumnTitle,
-  StyledFeedbackJoinCoreSection
+  StyledFeedbackJoinCoreSection,
+  StyledOceanLayers
 } from './FeedbackJoinCoreSection.styles';
 import PrimaryButton from '../../../shared-ui/components/primary-button/PrimaryButton';
+import { P } from '../../../shared-ui/style/typography';
+import OceanLayers from '../../../shared-ui/images/OceanLayers.svg';
 
 const FeedbackJoinCoreSection: React.FC = () => {
   return (
-    <StyledFeedbackJoinCoreSection>
+    <><StyledFeedbackJoinCoreSection>
       <StyledColumn>
         <StyledColumnTitle>Send us your feedback</StyledColumnTitle>
-        <StyledColumnText>
-          We hope you{"'"}re having an awesome time at HackBeanpot Desert Exploration! We{"'"}
+        <P>
+          We hope you{"'"}re having an awesome time at HackBeanpot Under the Sea! We{"'"}
           d really appreciate it if you could share your thoughts in our Event
           Feedback Form below so we can continue to make HackBeanpot a great
           experience for all attendees.
-        </StyledColumnText>
+        </P>
         <PrimaryButton
           btnText="Send Feedback"
           btnLink="https://forms.gle/Fw8vXzJuzx4h1yvJA"
-          newTab
-        />
+          newTab />
       </StyledColumn>
       <StyledColumn>
         <StyledColumnTitle>Apply to join Core!</StyledColumnTitle>
-        <StyledColumnText>
+        <P>
           Interested in learning more about organizing a hackathon? Join our
           HackBeanpot Core Team to develop your skills in tech, design,
           marketing, or leadership! We ask that you be able to commit about 10
           hours per week, but it{"'"}s definitely a fun time with a group of
           inspiring and friendly people!
-        </StyledColumnText>
+        </P>
         <PrimaryButton
           btnText="Apply"
           btnLink="https://docs.google.com/forms/d/e/1FAIpQLScyEQzS4RBkkuPSC4cRlVFUYqnu0mkAcC4ypA21aR4aC2C6LQ/viewform"
-          newTab
-        />
+          newTab />
       </StyledColumn>
-    </StyledFeedbackJoinCoreSection>
+    </StyledFeedbackJoinCoreSection><StyledOceanLayers src={OceanLayers} /></>
+
   );
 };
 
