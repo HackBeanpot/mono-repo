cea5730d619bc556aac700282599513da616515e
jmsargent10 <sargent.j@northeastern.edu>
2024-05-13 23:25:23 -0400

    making fresh branch for changes

--- main-site/components/events-calendar-section/EventsCalendarSection.tsx
+++ main-site/components/events-calendar-section/EventsCalendarSection.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import EventsSeaweed from '../../../shared-ui/images/seaweed-rock.svg';
-import EventsSeaweedDark from '../../../shared-ui/images/seaweed-rock-dark.svg';
 import EventsFishSchool from '../../../shared-ui/images/school-fish.svg';
 import EventsFishSchoolDark from '../../../shared-ui/images/school-fish-dark.svg';
 
 import {
-  StyledEventsSeaweed,
   StyledEventsFishSchool,
   StyledEventsContainer,
   StyledEventsCalendar,
@@ -15,7 +12,6 @@ import {
 import { min } from '../../../shared-ui/lib/responsive';
 import useMatchMedia from 'react-use-match-media';
 import DesktopTexts from './texts/DesktopTexts';
-import { eventsCalendarData } from '../../lib/data';
 
 const EventsCalendarSection: React.FC = ({ isDay }) => {
   const isDesktop = useMatchMedia(min.tablet);
