5e626bcf9caf585a68811baeab97c480fe15326e
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2024-06-02 17:04:17 -0400

    adding styling and responsiveness to NoUpcoming

--- live-site/components/coming-up/no-upcoming/NoUpcoming.styles.tsx
+++ live-site/components/coming-up/no-upcoming/NoUpcoming.styles.tsx
@@ -3,15 +3,29 @@ import { max } from '../../../../shared-ui/lib/responsive';
 import { colors } from '../../../../shared-ui/style/colors';
 import { H2 } from '../../../../shared-ui/style/typography';
 
+
+
 const NoUpcomingContainer = styled.div`
+  border-radius: 2em;
+  background-color: ${colors.YELLOW_GREEN};
+  padding: 5em 0 5em 0;
+  width: 60%;
   text-align: center;
-  padding-top: 20em;
-  padding-bottom: 15em;
+  margin: 10em auto 0;
+
+  @media ${max.tabletLg} {
+        width: 80%;
+        padding: 5em 0 5em 0;
+      }
+      @media ${max.tablet} {
+        border-radius: 1.3em;
+        width: 90%;
+      }
 `;
 
 const StyledH2 = styled(H2)`
   letter-spacing: 0.01em;
-  font-size: 2.5em;
+  font-size: 2em;
   color: ${colors.TEXT_BROWN};
   @media ${max.tablet} {
     font-size: 2em;
